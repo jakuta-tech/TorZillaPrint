29e570cc48477098948d16d55b106afdbb3139fe
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-03-13 14:03:55 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -8,16 +8,8 @@ dom.nOnLine = navigator.onLine;
 /* network information api: dom.netinfo.enabled */
 if ("connection" in navigator) {
   dom.nNetwork = "enabled";
-  
-  // only type is returned in desktop
-  document.getElementById("nConnection").innerHTML =
-  "type: "+ navigator.connection.type + "<br>"+
-  "downlink: "+ navigator.connection.downlink +`<br>`+
-  "downlinkMax: "+ navigator.connection.downlinkMax +`<br>`+
-  "effectiveType: "+ navigator.connection.effectiveType +`<br>`+
-  "rtt: "+ navigator.connection.rtt;
+  dom.nConnection = navigator.connection.type;
 } else {
   dom.nNetwork = "disabled";
-  // desktop/android: with/without RFP = "undefined"
   dom.nConnection = navigator.connection;
 };
