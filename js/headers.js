813fd61752dadd365f1f6dd913bb7d9220057c7d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-03-13 12:21:37 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -2,6 +2,16 @@
 
 'use strict';
 
-dom.nConnection = navigator.connection;
 dom.nDoNotTrack = navigator.doNotTrack;
 dom.nOnLine = navigator.onLine;
+
+/* network information api: dom.netinfo.enabled */
+if ("connection" in navigator) {
+  dom.nNetwork = "enabled";
+  // desktop: with/without RFP = "[object NetworkInformation]"
+  dom.nConnection = navigator.connection;
+} else {
+  dom.nNetwork = "disabled";
+  // desktop: with/without RFP = "undefined"
+  dom.nConnection = navigator.connection;
+};
