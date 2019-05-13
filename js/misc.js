c6ba105cf89f2e9605321ec99a3c783e158ee88c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-13 10:26:57 +0000

    Update misc.js

--- js/misc.js
+++ js/misc.js
@@ -2,21 +2,30 @@
 
 'use strict';
 
-/* mathml */
-// use empty clipbooard as it has no tooltip which affects the height
-var mathml1 = document.getElementById("nClipboard").offsetHeight;
-var mathml2 = document.getElementById("mathml2").offsetHeight;
-if ( mathml1 == mathml2) {dom.mathml1="disabled"} else {dom.mathml1="enabled"};
+function outputMisc() {
+  /* mathml */
+  // reset the test (required for a section re-run: although it actually requires a page refresh)
+  var mathmlString = "<math><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo form='prefix'>&minus;</mo><mi>b</mi>"+
+    "<mo>&PlusMinus;</mo><msqrt><msup><mi>b</mi><mn>2</mn></msup><mo>&minus;</mo><mn>4</mn>"+
+    "<mo>&InvisibleTimes;</mo><mi>a</mi><mo>&InvisibleTimes;</mo><mi>c</mi></msqrt></mrow>"+
+    "<mrow><mn>2</mn><mo>&InvisibleTimes;</mo><mi>a</mi></mrow></mfrac></mrow></math>"
+  document.getElementById("mathmltest").innerHTML = mathmlString;
+  // control must be visible and no distortion of height e.g tooltips or buttons
+  var mathmlnone = document.getElementById("pReducedMotion").offsetHeight;
+  var mathmltest = document.getElementById("mathmltest").offsetHeight;
+  if ( mathmltest == mathmlnone) {dom.mathml="disabled"} else {dom.mathml="enabled"};
+  console.log ("mathmlnone: " + mathmlnone + "; mathmltest: " + mathmltest);
 
-/* clipboard */
-if ("clipboard" in navigator) {dom.nClipboard="enabled"} else {dom.nClipboard="disabled"};
+  /* clipboard */
+  if ("clipboard" in navigator) {dom.nClipboard="enabled"} else {dom.nClipboard="disabled"};
+  /* Intersection Observer api (dom.IntersectionObserver.enabled) */
+  var callback = function(entries, observer) {};
+  try {
+    var observer = new IntersectionObserver(callback);
+    dom.intObserver="enabled"
+  } catch(e) {dom.intObserver="disabled" };
+  /* requestIdleCallback (dom.requestIdleCallback.enabled) */
+  if ("requestIdleCallback" in window) {dom.reqIdleCB="enabled"} else {dom.reqIdleCB="disabled"};
+};
 
-/* Intersection Observer api (dom.IntersectionObserver.enabled) */
-var callback = function(entries, observer) {};
-try {
-  var observer = new IntersectionObserver(callback);
-  dom.intObserver="enabled"
-} catch(e) {dom.intObserver="disabled" };
-
-/* requestIdleCallback (dom.requestIdleCallback.enabled) */
-if ("requestIdleCallback" in window) {dom.reqIdleCB="enabled"} else {dom.reqIdleCB="disabled"};
+outputMisc();
