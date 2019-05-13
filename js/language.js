d01cc1533f62af7f01230ba9e582f9d4251d317d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-13 20:15:55 +0000

    Update language.js

--- js/language.js
+++ js/language.js
@@ -77,13 +77,9 @@ function outputLanguage() {
     document.getElementById("iframeDTD2").contentWindow.postMessage('foo', '*');
   };
   // DTD combined
-  // to fix: why does dtd1 not repopulate on a "rerun"
   setTimeout(function(){
     if (dtd1 == "") {dom.localeDTD = dtd2} else {dom.localeDTD = dtd1 + " | " + dtd2}
-    //iframeDTD1.src="";
-    //iframeDTD2.src="";
   }, 1000);
-
 };
 
 outputLanguage();
