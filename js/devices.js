659cca0e2379d347b491fa4637fee9cfd68fd1ad
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-13 09:09:03 +0000

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -49,35 +49,6 @@ function outputDevices() {
     dom.nGetVR="disabled";
     dom.nActiveVR="n/a"; // always use n/a for skipped tests
   };
-  // system colors
-  var sColorStr = ""; var clrValue = "";
-  var sColorArray = ['ActiveBorder', 'ActiveCaption', 'AppWorkspace', 'Background', 'ButtonFace', 'ButtonHighlight', 'ButtonShadow',
-    'ButtonText', 'CaptionText', 'GrayText', 'Highlight', 'HighlightText', 'InactiveBorder', 'InactiveCaption', 'InactiveCaptionText',
-    'InfoBackground', 'InfoText', 'Menu', 'MenuText', 'Scrollbar', 'ThreeDDarkShadow','ThreeDFace', 'ThreeDHighlight',
-    'ThreeDLightShadow', 'ThreeDShadow', 'Window', 'WindowFrame', 'WindowText'];
-  var sColorElem = document.getElementById("sColorElement");
-  sColorArray.forEach(function (arrayItem) {
-    sColorElem.style.backgroundColor = arrayItem;
-    sColorStr = sColorStr + window.getComputedStyle(sColorElem, null).getPropertyValue("background-color")
-  });
-  dom.sColorHash = sha1(sColorStr);
-  // prefers-color-scheme
-  var pColorElem = document.getElementById("pColorElement");
-  var pRGB = window.getComputedStyle(pColorElem, null).getPropertyValue("background-color");
-  if (pRGB == "rgb(255, 255, 255)") {pRGB = "light"}
-  else if (pRGB == "rgb(255, 0, 0)") {pRGB = "not supported"}
-  else if (pRGB == "rgb(0, 0, 0)") {pRGB = "dark"}
-  else if (pRGB == "rgb(0, 0, 255)") {pRGB = "no-preference"};
-  dom.pColorScheme = pRGB;
-  // prefers-reduced-motion
-  var pRGB = "";
-  var pMotionElem = document.getElementById("pMotionElement");
-  var pRGB = window.getComputedStyle(pMotionElem, null).getPropertyValue("background-color");
-  if (pRGB == "rgb(255, 255, 255)") {pRGB = "no-preference"}
-  else if (pRGB == "rgb(255, 0, 0)") {pRGB = "not supported"}
-  else if (pRGB == "rgb(0, 0, 0)") {pRGB = "reduce"};
-  dom.pReducedMotion = pRGB;
-
 };
 
 outputDevices();
