aef786be9eed5af9198bdc5afb14bf7230961c3c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-14 21:24:00 +0000

    Update screen.js

--- js/screen.js
+++ js/screen.js
@@ -485,7 +485,7 @@ if (isNaN(window.mozPaintCount) === false){
       if (lh=="19.1667") {lhOS=strW};
       if (lh=="19") {lhOS=strTBL};
       if (lh=="18.3333") {lhOS=strWL};
-      if (lh=="17.6667") {lhOS=strWL};
+      if (lh=="17.5") {lhOS=strL};
     } else if (jsZoom == 200) {
       if (lh=="19") {lhOS=strW};
       if (lh=="18") {lhOS=strL};
@@ -516,10 +516,10 @@ if (isNaN(window.mozPaintCount) === false){
       if (lh=="18.3333") {lhOS=strL};
       if (lh=="17.4167") {lhOS=strL};
     } else if (jsZoom == 100) {
-        if (lh=="20") {lhOS=strW};
-        if (lh=="19") {lhOS=strL};
-        if (lh=="18") {lhOS=strW};
-        if (lh=="17") {lhOS=strL};
+      if (lh=="20") {lhOS=strW};
+      if (lh=="19") {lhOS=strL};
+      if (lh=="18") {lhOS=strW};
+      if (lh=="17") {lhOS=strL};
     } else if (jsZoom == 90) {
       if (lh=="20.1") {lhOS=strW};
       if (lh=="18.9833") {lhOS=strWL};
@@ -545,10 +545,14 @@ if (isNaN(window.mozPaintCount) === false){
   };
   // detect MAC
   if (lhOS == "") {
-    /* mac unique decimal points: .0167 .1833 .35 .4333 .6833 .85
-       mac not unique: but still test because lhOS is blank: .7667 .6667
+    /*     mac unique: .0167 .1833 .35 .4333 .6833 .85
+       mac not unique: .7667 .6667 (but unique at those zoom values)
+       6667 -  mac 90 - WIN 30 + 150 + 170 + 300, LINUX 150
+       7667 - mac 170 - TB 90
        19.5167 : never seen this in testing: was listed in an that hackernews forum */
-    
+    var lhDec = (lh+"").split(".")[1];
+    if (lhDec=="0167" | lhDec=="1833" | lhDec=="35" | lhDec=="4333" | lhDec=="6833"
+      | lhDec=="85" | lhDec=="7667" | lhDec=="6667" | lhDec=="5167") {lhOS=strM};
   };
   // detect ANDROID
   if (lhOS == "") {
@@ -558,10 +562,9 @@ if (isNaN(window.mozPaintCount) === false){
       lhOS = strA
     } else {
       // if still blank test/improve zoom by looking at scrollbar width
-      
     };
   };
-  // best guess or known metric
+  // still blank? and add logical guess or known metric
   if (lhOS == "") {
     lhOS = "[Linux] [logical guess]"
   } else {
