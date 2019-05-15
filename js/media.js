134197e599ff0f54438b440b999674b833925c02
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-15 20:35:47 +0000

    Update media.js

--- js/media.js
+++ js/media.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+// media.media-capabilities.enabled
 if ("mediaCapabilities" in navigator) {dom.nMediaCapabilities="enabled"}
-  else {if (Symbol.for(`foo`).description == "foo"){dom.nMediaCapabilities="disabled"}
-    else {dom.nMediaCapabilities="not supported"};};
+	else {if (Symbol.for(`foo`).description == "foo"){dom.nMediaCapabilities="disabled"}
+		else {dom.nMediaCapabilities="not supported"};};
