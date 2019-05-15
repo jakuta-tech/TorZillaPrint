f7f3b51e1387483d02fb4e5e21741b133798a045
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-15 10:42:16 +0000

    Update css.js

--- js/css.js
+++ js/css.js
@@ -6,19 +6,19 @@ function outputCSS() {
   // prefers-color-scheme
   var pColorElem = document.getElementById("pColorElement");
   var pRGB = window.getComputedStyle(pColorElem, null).getPropertyValue("background-color");
-  if (pRGB == "rgb(255, 255, 255)") {pRGB = "light"}
+  if (pRGB == "rgb(255, 255, 255)") {pRGB = "light"+RFPy}
   else if (pRGB == "rgb(255, 0, 0)") {pRGB = "not supported"}
-  else if (pRGB == "rgb(0, 0, 0)") {pRGB = "dark"}
-  else if (pRGB == "rgb(0, 0, 255)") {pRGB = "no-preference"};
-  dom.pColorScheme = pRGB;
+  else if (pRGB == "rgb(0, 0, 0)") {pRGB = "dark"+RFPn}
+  else if (pRGB == "rgb(0, 0, 255)") {pRGB = "no-preference"+RFPn};
+  dom.pColorScheme.innerHTML = pRGB;
   // prefers-reduced-motion
   var pRGB = "";
   var pMotionElem = document.getElementById("pMotionElement");
   var pRGB = window.getComputedStyle(pMotionElem, null).getPropertyValue("background-color");
-  if (pRGB == "rgb(255, 255, 255)") {pRGB = "no-preference"}
+  if (pRGB == "rgb(255, 255, 255)") {pRGB = "no-preference"+RFPy}
   else if (pRGB == "rgb(255, 0, 0)") {pRGB = "not supported"}
-  else if (pRGB == "rgb(0, 0, 0)") {pRGB = "reduce"};
-  dom.pReducedMotion = pRGB;
+  else if (pRGB == "rgb(0, 0, 0)") {pRGB = "reduce"+RFPn};
+  dom.pReducedMotion.innerHTML = pRGB;
   // system colors
   var sColorStr = ""; var clrValue = "";
   var sColorArray = ['ActiveBorder', 'ActiveCaption', 'AppWorkspace', 'Background', 'ButtonFace', 'ButtonHighlight', 'ButtonShadow',
@@ -30,7 +30,11 @@ function outputCSS() {
     sColorElem.style.backgroundColor = arrayItem;
     sColorStr = sColorStr + window.getComputedStyle(sColorElem, null).getPropertyValue("background-color")
   });
-  dom.sColorHash = sha1(sColorStr);
+  sColorHash = sha1(sColorStr);
+  if (sColorHash == "b5e2344c265fc498d2fb8e0f84951e8d501ad481") {
+    sColorHash = sColorHash + RFPy
+  } else {sColorHash = sColorHash + RFPn};
+  dom.sColorHash.innerHTML = sColorHash;
 };
 
 outputCSS();
