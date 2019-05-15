f7acecffda37b9cafe0a78bc7773204c04fff821
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-15 20:44:07 +0000

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -3,52 +3,42 @@
 'use strict';
 
 function outputDevices() {
-  // navigator
-  dom.nHardwareConcurrency = navigator.hardwareConcurrency;
-  dom.nMaxTouchPoints = navigator.maxTouchPoints;
-  if (navigator.getGamepads) {dom.nGetGamepads="enabled"} else {dom.nGetGamepads="disabled"};
-  // media devices (media.navigator.enabled)
-  if ("mediaDevices" in navigator) {
-    dom.nMediaDevices="enabled";
-    // enumerate media devices
-    try {
-      // don't do if insecure or file?
-      //navigator.mediaDevices.getUserMedia({audio:true});
-      dom.eMediaDevices="yes: test to come";
-    }
-    catch(e) {dom.eMediaDevices="no: " + e.name};
-  }
-  else {
-    dom.nMediaDevices="disabled";
-    dom.eMediaDevices="n/a"; // always use n/a for skipped tests
-  };
-  // media.webspeech.synth.enabled
-  if ("speechSynthesis" in window) {
-    dom.speechSynth="enabled"
-    // speech engines
-    //   ^^ https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
-    // get properties of each voice: eg SpeechSynthesisVoice.default 
-    //   ^^ https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice   
-    // dom.speechEngines = speechSynthesis.getVoices().map(v => v.name+"|"+v.lang);
-    dom.speechEngines="yes: test to come";
-  }
-  else {
-    dom.speechSynth="disabled";
-    dom.speechEngines="n/a"; // always use n/a for skipped tests
-  };
-  // VR (dom.vr.enabled)
-  if ("getVRDisplays" in navigator) {
-    dom.nGetVR="enabled";
-    // active
-    if ("activeVRDisplays" in navigator) {
-      if (navigator.activeVRDisplays == "") {dom.nActiveVR="[empty array]"}
-      else {dom.nActiveVR=navigator.activeVRDisplays};
-    };
-  }
-  else {
-    dom.nGetVR="disabled";
-    dom.nActiveVR="n/a"; // always use n/a for skipped tests
-  };
+	// navigator
+	dom.nHardwareConcurrency = navigator.hardwareConcurrency;
+	dom.nMaxTouchPoints = navigator.maxTouchPoints;
+	if (navigator.getGamepads) {dom.nGetGamepads="enabled"} else {dom.nGetGamepads="disabled"};
+	// media devices (media.navigator.enabled)
+	if ("mediaDevices" in navigator) {
+		dom.nMediaDevices="enabled";
+		// enumerate media devices
+		try {
+			dom.eMediaDevices="yes: test to come";
+		}
+		catch(e) {dom.eMediaDevices="no: " + e.name};
+	}
+	else {
+		dom.nMediaDevices="disabled";	dom.eMediaDevices="n/a";
+	};
+	// media.webspeech.synth.enabled
+	if ("speechSynthesis" in window) {
+		dom.speechSynth="enabled"
+		dom.speechEngines="yes: test to come";
+	}
+	else {
+		dom.speechSynth="disabled";	dom.speechEngines="n/a";
+	};
+	// VR (dom.vr.enabled)
+	if ("getVRDisplays" in navigator) {
+		dom.nGetVR="enabled";
+		// active
+		if ("activeVRDisplays" in navigator) {
+			if (navigator.activeVRDisplays == "") {dom.nActiveVR="[empty array]"}
+			else {dom.nActiveVR=navigator.activeVRDisplays};
+		};
+	}
+	else {
+		dom.nGetVR="disabled"; dom.nActiveVR="n/a";
+	};
 };
 
 outputDevices();
