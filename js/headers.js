64912eb3f9569fc3bedd829ea735a5efa609e9da
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-16 09:21:56 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -2,12 +2,15 @@
 
 'use strict';
 
-dom.nDoNotTrack = navigator.doNotTrack;
-dom.nOnLine = navigator.onLine;
-
-// network info api: dom.netinfo.enabled
-if ("connection" in navigator) {
-	dom.nNetwork = "enabled"; dom.nConnection = navigator.connection.type;
-} else {
-	dom.nNetwork = "disabled"; dom.nConnection = navigator.connection;
+function outputHeaders() {
+	dom.nDoNotTrack = navigator.doNotTrack;
+	dom.nOnLine = navigator.onLine;
+	// network info api: dom.netinfo.enabled
+	if ("connection" in navigator) {
+		dom.nNetwork = "enabled"; dom.nConnection = navigator.connection.type;
+	} else {
+		dom.nNetwork = "disabled"; dom.nConnection = navigator.connection;
+	};
 };
+
+outputHeaders();
