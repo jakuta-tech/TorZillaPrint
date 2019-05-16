e6af57e2ad3507cce9eb82ccf9fdeac7068e21c0
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-16 09:21:19 +0000

    Update media.js

--- js/media.js
+++ js/media.js
@@ -2,7 +2,11 @@
 
 'use strict';
 
-// media.media-capabilities.enabled
-if ("mediaCapabilities" in navigator) {dom.nMediaCapabilities="enabled"}
-	else {if (Symbol.for(`foo`).description == "foo"){dom.nMediaCapabilities="disabled"}
-		else {dom.nMediaCapabilities="not supported"};};
+function outputMedia() {
+	// media.media-capabilities.enabled
+	if ("mediaCapabilities" in navigator) {dom.nMediaCapabilities="enabled"}
+		else {if (Symbol.for(`foo`).description == "foo"){dom.nMediaCapabilities="disabled"}
+			else {dom.nMediaCapabilities="not supported"};};
+};
+
+outputMedia();
