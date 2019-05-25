5830c5703d18c26283f6e05c4c1e57a741c22ab0
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-25 22:21:21 +0000

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -17,7 +17,8 @@ var TBy=" <span class='good'>[TB]</span>"; // u2713
 var TBn=" <span class='bad'>[TB]</span>"; // u2715
 var RFPy=" <span class='good'>[RFP]</span>";
 var RFPn=" <span class='bad'>[RFP]</span>";
-var FILEy=" <span class='bad'>[file:]</span>"
+var FILEy=" <span class='ttc'>[file:]</span>"
+var TTC=" <span class='ttc'>test to come</span>"
 var amFF = false;
 if (isNaN(window.mozPaintCount) === false) {amFF = true};
 
