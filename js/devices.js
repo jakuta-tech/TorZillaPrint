8cdcef334d41f469a8228b07037ba354704df221
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-05-25 22:27:38 +0000

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -3,7 +3,8 @@
 'use strict';
 
 function outputDevices() {
-	// navigator
+
+	/*** navigator */
 	var tmpD = navigator.hardwareConcurrency;
 	if (tmpD == "2") {tmpD = tmpD + RFPy} else {tmpD = tmpD + RFPn}
 	dom.nHardwareConcurrency.innerHTML = tmpD;
@@ -11,30 +12,40 @@ function outputDevices() {
 	if (tmpD == "0") {tmpD = tmpD + RFPy} else {tmpD = tmpD + RFPn}
 	dom.nMaxTouchPoints.innerHTML = tmpD;
 	if (navigator.getGamepads) {dom.nGetGamepads="enabled"} else {dom.nGetGamepads="disabled"};
-	// media devices (media.navigator.enabled)
+
+	/*** media devices (media.navigator.enabled) */
 	if ("mediaDevices" in navigator) {
 		dom.nMediaDevices="enabled";
-		// enumerate media devices
+		/*** enumerate media devices */
 		try {
-			dom.eMediaDevices="yes: test to come";
+			// don't do if insecure or file?
+			// navigator.mediaDevices.getUserMedia({audio:true});
+			dom.eMediaDevices=TTC;
 		}
 		catch(e) {dom.eMediaDevices="no: " + e.name};
 	}
 	else {
 		dom.nMediaDevices="disabled";	dom.eMediaDevices="n/a";
 	};
-	// media.webspeech.synth.enabled
+
+	/*** media.webspeech.synth.enabled */
 	if ("speechSynthesis" in window) {
 		dom.speechSynth="enabled"
-		dom.speechEngines="yes: test to come";
+		/*** speech engines */
+		//	 ^^ https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
+		// get properties of each voice: eg SpeechSynthesisVoice.default 
+		//	 ^^ https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice		
+		// dom.speechEngines = speechSynthesis.getVoices().map(v => v.name+"|"+v.lang);
+		dom.speechEngines=TTC;
 	}
 	else {
 		dom.speechSynth="disabled";	dom.speechEngines="n/a";
 	};
-	// VR (dom.vr.enabled)
+
+	/*** VR (dom.vr.enabled) */
 	if ("getVRDisplays" in navigator) {
 		dom.nGetVR="enabled";
-		// active
+		/*** active */
 		if ("activeVRDisplays" in navigator) {
 			if (navigator.activeVRDisplays == "") {dom.nActiveVR="[empty array]"}
 			else {dom.nActiveVR=navigator.activeVRDisplays};
@@ -43,6 +54,7 @@ function outputDevices() {
 	else {
 		dom.nGetVR="disabled"; dom.nActiveVR="n/a";
 	};
+
 };
 
 outputDevices();
