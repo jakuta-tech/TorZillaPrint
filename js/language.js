fb5d11a9f10fe8b6cabac596d4e5a50ce703ccbf
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-06-08 07:12:30 +0000

    Update language.js

--- js/language.js
+++ js/language.js
@@ -63,8 +63,12 @@ function outputLanguage() {
 	// geo
 	if ("geolocation" in navigator) {dom.nGeolocation="yes"} else (dom.nGeolocation="no");
 	navigator.permissions.query({name:"geolocation"}).then(e => dom.pGeolocation=e.state);
-	// DTD locale test 1
+
+	/*** leak PoCs */
 	if (amFF == true) {
+		// dom.properties
+		dom.appLang1 = document.getElementById("appL1el").validationMessage;
+		// dtd
 		var dtd1 = ""; var dtd2="";
 		const iframeDTD1 = document.getElementById("iframeDTD1");
 		iframeDTD1.src="iframes/dtdlocale.xml";
@@ -73,7 +77,7 @@ function outputLanguage() {
 			dtd1 = iframeDTD1.contentDocument.getElementById("DTD1").innerText;
 			window.removeEventListener("load", dtdlocale1)
 		};
-		// DTD locale test 2
+		// dtd nullprinciple
 		const iframeDTD2 = document.getElementById("iframeDTD2");
 		iframeDTD2.src="data:application/xml;charset=utf-8,%3C%21DOCTYPE%20html%20SYSTEM%20%22chrome%3A%2F%2Fglobal%2Flocale%2FnetError.dtd%22%3E%3Chtml%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxhtml%22%3E%3Chead%3E%3Cmeta%20charset%3D%22utf-8%22%2F%3E%0D%0A%20%20%3C%2Fhead%3E%0D%0A%20%20%3Cbody%3E%3Cspan%20id%3D%22text-container%22%3E%26loadError.label%3B%3C%2Fspan%3E%0D%0A%20%20%3Cscript%3E%0D%0A%20%20window.addEventListener%28%27message%27%2C%20%28e%29%20%3D%3E%20%7B%0D%0A%20%20%20%20e.source.postMessage%28document.getElementById%28%27text-container%27%29.innerText%2C%20%27%2A%27%29%3B%0D%0A%20%20%7D%29%3B%0D%0A%20%20%3C%2Fscript%3E%0D%0A%20%20%3C%2Fbody%3E%0D%0A%3C%2Fhtml%3E";
 		iframeDTD2.addEventListener("load", dtdlocale2)
@@ -81,11 +85,23 @@ function outputLanguage() {
 			window.addEventListener('message', ({ data }) => dtd2 = data);
 			document.getElementById("iframeDTD2").contentWindow.postMessage('foo', '*');
 		};
-		// DTD combined
 		setTimeout(function(){
-			if (dtd1 == "") {dom.localeDTD = dtd2} else {dom.localeDTD = dtd1 + " | " + dtd2}
+			dom.appLang2 = dtd1; dom.appLang3 = dtd2;
 		}, 1000);
+		// MediaDocument.properties
+		const iframeAPPL = document.getElementById("iframeAPPL");
+		iframeAPPL.src="images/dummy.png";
+		iframeAPPL.addEventListener("load", function() {
+			dom.appLang4 = (this.contentWindow.document.title);
+		});
+		// xmlparser.properties
+		const appL5doc = (new DOMParser).parseFromString('getyourlocale', 'application/xhtml+xml');
+		var str2 = (appL5doc.getElementsByTagName('parsererror')[0].firstChild.textContent);
+		var nb1 = str2.search("http"); if (nb1 == -1) {nb1 = str2.search("file")}
+		var nb2 = str2.search("html") + 4;
+		dom.appLang5 = str2.slice(0,nb1-1) + str2.slice(nb2);
 	};
+
 };
 
 outputLanguage();
