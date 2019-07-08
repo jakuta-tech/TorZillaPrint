68546aca162f1e2dbb0c3149043b8b3d3d987857
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-08 10:24:00 +0000

    Update language.js

--- js/language.js
+++ js/language.js
@@ -6,15 +6,14 @@
 function cleanify(data){
 	return data.map(function(entry){return entry.value;}).join("");
 };
-// date/time variables
-var dateUsed = new Date("January 30, 2019 13:00:00");
-var dateOld = new Date("July 30, 2018 13:00:00");
-var dateFormatted = new Intl.DateTimeFormat(undefined, { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric", hour12: true, timeZoneName: "long" });
-var rOptions = dateFormatted.resolvedOptions();
-// date/time format options
-var dateOpt = { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric", hour12: true, timeZoneName: "long" };
 
 function outputLanguage() {
+	// variables
+	let dateUsed = new Date("January 30, 2019 13:00:00");
+	let dateOld = new Date("July 30, 2018 13:00:00");
+	let dateFormatted = new Intl.DateTimeFormat(undefined, { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric", hour12: true, timeZoneName: "long" });
+	let rOptions = dateFormatted.resolvedOptions();
+	let dateOpt = { weekday: "long", month: "long", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric", hour12: true, timeZoneName: "long" };
 	// language
 	dom.nLanguages = navigator.languages;
 	dom.nLanguage = navigator.language;
@@ -23,7 +22,7 @@ function outputLanguage() {
 	dom.localeIPR = new Intl.PluralRules().resolvedOptions().locale;
 	dom.localeRO = rOptions.locale;
 	// timezone
-	var tmpL = dateUsed.getTimezoneOffset()+ ' | ' + dateOld.getTimezoneOffset();
+	let tmpL = dateUsed.getTimezoneOffset()+ ' | ' + dateOld.getTimezoneOffset();
 	if (tmpL == "0 | 0") {tmpL = tmpL + RFPy} else {tmpL = tmpL + RFPn};
 	dom.tzOffsets.innerHTML = tmpL;
 	tmpL = Intl.DateTimeFormat().resolvedOptions().timeZone;
@@ -69,8 +68,8 @@ function outputLanguage() {
 		// dom.properties
 		dom.appLang1 = document.getElementById("appL1el").validationMessage;
 		// dtd
-		var dtd1 = ""; var dtd2="";
-		const iframeDTD1 = document.getElementById("iframeDTD1");
+		let dtd1 = "", dtd2="";
+		let iframeDTD1 = document.getElementById("iframeDTD1");
 		iframeDTD1.src="iframes/dtdlocale.xml";
 		iframeDTD1.addEventListener("load", dtdlocale1)
 		function dtdlocale1() {
@@ -78,7 +77,7 @@ function outputLanguage() {
 			window.removeEventListener("load", dtdlocale1)
 		};
 		// dtd nullprinciple
-		const iframeDTD2 = document.getElementById("iframeDTD2");
+		let iframeDTD2 = document.getElementById("iframeDTD2");
 		iframeDTD2.src="data:application/xml;charset=utf-8,%3C%21DOCTYPE%20html%20SYSTEM%20%22chrome%3A%2F%2Fglobal%2Flocale%2FnetError.dtd%22%3E%3Chtml%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxhtml%22%3E%3Chead%3E%3Cmeta%20charset%3D%22utf-8%22%2F%3E%0D%0A%20%20%3C%2Fhead%3E%0D%0A%20%20%3Cbody%3E%3Cspan%20id%3D%22text-container%22%3E%26loadError.label%3B%3C%2Fspan%3E%0D%0A%20%20%3Cscript%3E%0D%0A%20%20window.addEventListener%28%27message%27%2C%20%28e%29%20%3D%3E%20%7B%0D%0A%20%20%20%20e.source.postMessage%28document.getElementById%28%27text-container%27%29.innerText%2C%20%27%2A%27%29%3B%0D%0A%20%20%7D%29%3B%0D%0A%20%20%3C%2Fscript%3E%0D%0A%20%20%3C%2Fbody%3E%0D%0A%3C%2Fhtml%3E";
 		iframeDTD2.addEventListener("load", dtdlocale2)
 		function dtdlocale2() {
@@ -89,21 +88,18 @@ function outputLanguage() {
 			dom.appLang2 = dtd1; dom.appLang3 = dtd2;
 		}, 1500);
 		// MediaDocument.properties
-		const iframeAPPL = document.getElementById("iframeAPPL");
+		let iframeAPPL = document.getElementById("iframeAPPL");
 		iframeAPPL.src="images/dummy.png";
 		iframeAPPL.addEventListener("load", function() {
 			dom.appLang4 = (this.contentWindow.document.title);
 		});
 		// xmlparser.properties
-		const appL5doc = (new DOMParser).parseFromString('getyourlocale', 'application/xhtml+xml');
-		var str2 = (appL5doc.getElementsByTagName('parsererror')[0].firstChild.textContent);
-		//console.log("str2 before: "+str2);
-		var nb1 = str2.search("http"); if (nb1 == -1) {nb1 = str2.search("file")}
-		var nb2 = str2.search("html") + 4;
-		//console.log("str2 after : " + str2.slice(0,nb1-1) + str2.slice(nb2) );
+		let appL5doc = (new DOMParser).parseFromString('getyourlocale', 'application/xhtml+xml');
+		let str2 = (appL5doc.getElementsByTagName('parsererror')[0].firstChild.textContent);
+		let nb1 = str2.search("http"); if (nb1 == -1) {nb1 = str2.search("file")}
+		let nb2 = str2.search("html") + 4;
 		dom.appLang5 = str2.slice(0,nb1-1) + str2.slice(nb2);
 	};
-
 };
 
 outputLanguage();
