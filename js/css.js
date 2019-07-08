0441fc163303830511023bfc4378caee93e612e3
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-08 10:38:13 +0000

    Update css.js

--- js/css.js
+++ js/css.js
@@ -3,38 +3,43 @@
 'use strict';
 
 function outputCSS() {
+	let pRGB = "";
+
 	// prefers-color-scheme
-	var pColorElem = document.getElementById("pColorElement");
-	var pRGB = window.getComputedStyle(pColorElem, null).getPropertyValue("background-color");
+	let pColorElem = document.getElementById("pColorElement");
+	pRGB = window.getComputedStyle(pColorElem, null).getPropertyValue("background-color");
 	if (pRGB == "rgb(255, 255, 255)") {pRGB = "light"+RFPy}
 	else if (pRGB == "rgb(255, 0, 0)") {pRGB = "not supported"}
 	else if (pRGB == "rgb(0, 0, 0)") {pRGB = "dark"+RFPn}
 	else if (pRGB == "rgb(0, 0, 255)") {pRGB = "no-preference"+RFPn};
 	dom.pColorScheme.innerHTML = pRGB;
+
 	// prefers-reduced-motion
-	var pRGB = "";
-	var pMotionElem = document.getElementById("pMotionElement");
-	var pRGB = window.getComputedStyle(pMotionElem, null).getPropertyValue("background-color");
+	pRGB = "";
+	let pMotionElem = document.getElementById("pMotionElement");
+	pRGB = window.getComputedStyle(pMotionElem, null).getPropertyValue("background-color");
 	if (pRGB == "rgb(255, 255, 255)") {pRGB = "no-preference"+RFPy}
 	else if (pRGB == "rgb(255, 0, 0)") {pRGB = "not supported"}
 	else if (pRGB == "rgb(0, 0, 0)") {pRGB = "reduce"+RFPn};
 	dom.pReducedMotion.innerHTML = pRGB;
+
 	// system colors
-	var sColorStr = ""; var clrValue = "";
-	var sColorArray = ['ActiveBorder', 'ActiveCaption', 'AppWorkspace', 'Background', 'ButtonFace', 'ButtonHighlight', 'ButtonShadow',
+	let sColorStr = "", clrValue = "";
+	let sColorArray = ['ActiveBorder', 'ActiveCaption', 'AppWorkspace', 'Background', 'ButtonFace', 'ButtonHighlight', 'ButtonShadow',
 	'ButtonText', 'CaptionText', 'GrayText', 'Highlight', 'HighlightText', 'InactiveBorder', 'InactiveCaption', 'InactiveCaptionText',
 	'InfoBackground', 'InfoText', 'Menu', 'MenuText', 'Scrollbar', 'ThreeDDarkShadow','ThreeDFace', 'ThreeDHighlight',
 	'ThreeDLightShadow', 'ThreeDShadow', 'Window', 'WindowFrame', 'WindowText'];
-	var sColorElem = document.getElementById("sColorElement");
+	let sColorElem = document.getElementById("sColorElement");
 	sColorArray.forEach(function (arrayItem) {
 		sColorElem.style.backgroundColor = arrayItem;
 		sColorStr = sColorStr + window.getComputedStyle(sColorElem, null).getPropertyValue("background-color")
 	});
-	var sColorHash = sha1(sColorStr);
+	let sColorHash = sha1(sColorStr);
 	if (sColorHash == "b5e2344c265fc498d2fb8e0f84951e8d501ad481") {
 		sColorHash = sColorHash + RFPy
 	} else {sColorHash = sColorHash + RFPn};
 	dom.sColorHash.innerHTML = sColorHash;
+
 };
 
 outputCSS();
