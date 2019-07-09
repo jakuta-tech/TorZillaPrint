70387a9cde07e81ac02ccf4ca83e9f439331e28a
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-09 04:40:49 +0000

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -2,36 +2,77 @@
 
 'use strict';
 
+/* global variables: generic.js
+	RFPy:  green [RFP]
+	RFPn:    red [RFP]
+	TTC : yellow [test to come]
+*/
+
 function outputDevices() {
+	// multipurpose string
+	let str = "";
 
-	/*** navigator */
-	let tmpD = navigator.hardwareConcurrency;
-	if (tmpD == "2") {tmpD = tmpD + RFPy} else {tmpD = tmpD + RFPn}
-	dom.nHardwareConcurrency.innerHTML = tmpD;
-	tmpD = navigator.maxTouchPoints;
-	if (tmpD == "0") {tmpD = tmpD + RFPy} else {tmpD = tmpD + RFPn}
-	dom.nMaxTouchPoints.innerHTML = tmpD;
-	if (navigator.getGamepads) {dom.nGetGamepads="enabled"} else {dom.nGetGamepads="disabled"};
+	// hardwareConcurrency
+	str = navigator.hardwareConcurrency;
+	if (str == "2") {
+		str = str + RFPy
+	} else {
+		str = str + RFPn
+	};
+	dom.nHardwareConcurrency.innerHTML = str;
 
-	/*** media devices (media.navigator.enabled) */
-	if ("mediaDevices" in navigator) {
-		dom.nMediaDevices="enabled";
-		/*** enumerate media devices */
-		try {
-			// don't do if insecure or file?
-			// navigator.mediaDevices.getUserMedia({audio:true});
-			dom.eMediaDevices.innerHTML=TTC;
-		}
-		catch(e) {dom.eMediaDevices="no: " + e.name};
-	}
-	else {
-		dom.nMediaDevices="disabled";	dom.eMediaDevices="n/a";
+	// maxTouchPoints
+	str = "";
+	str = navigator.maxTouchPoints;
+	if (str == "0") {
+		str = str + RFPy
+	} else {
+		str = str + RFPn
+	};
+	dom.nMaxTouchPoints.innerHTML = str;
+
+	// gamepads
+	if (navigator.getGamepads) {
+		dom.nGetGamepads="enabled"
+	} else {
+		dom.nGetGamepads="disabled"
 	};
 
-	/*** media.webspeech.synth.enabled */
+	// media.navigator.enabled
+	if ("mediaDevices" in navigator) {
+		dom.nMediaDevices="enabled";
+		// enumerate media devices
+		try {
+			dom.eMediaDevices.innerHTML=TTC;
+		} catch(e) {
+			dom.eMediaDevices="no: " + e.name
+		};
+	}	else {
+		dom.nMediaDevices="disabled";
+		dom.eMediaDevices="n/a";
+	};
+
+	// dom.vr.enabled
+	if ("getVRDisplays" in navigator) {
+		dom.nGetVR="enabled";
+		// active VR displays
+		if ("activeVRDisplays" in navigator) {
+			if (navigator.activeVRDisplays == "") {
+				dom.nActiveVR="[empty array]"
+			} else {
+				dom.nActiveVR=navigator.activeVRDisplays
+			};
+		};
+	}
+	else {
+		dom.nGetVR="disabled";
+		dom.nActiveVR="n/a";
+	};
+
+	// media.webspeech.synth.enabled
 	if ("speechSynthesis" in window) {
 		dom.speechSynth="enabled"
-		/*** speech engines */
+		// speech engines
 		//	 ^^ https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
 		// get properties of each voice: eg SpeechSynthesisVoice.default 
 		//	 ^^ https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice		
@@ -40,19 +81,6 @@ function outputDevices() {
 	}
 	else {
 		dom.speechSynth="disabled"; dom.speechEngines="n/a";
-	};
-
-	/*** VR (dom.vr.enabled) */
-	if ("getVRDisplays" in navigator) {
-		dom.nGetVR="enabled";
-		/*** active */
-		if ("activeVRDisplays" in navigator) {
-			if (navigator.activeVRDisplays == "") {dom.nActiveVR="[empty array]"}
-			else {dom.nActiveVR=navigator.activeVRDisplays};
-		};
-	}
-	else {
-		dom.nGetVR="disabled"; dom.nActiveVR="n/a";
 	};
 
 };
