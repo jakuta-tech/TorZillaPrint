4ea46320b183f735c7f27a86b5fd30e0d472742d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-09 05:10:43 +0000

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -7,20 +7,11 @@ function getUniqueElements() {
 			return obj[prop];
 		},
 		set: function(obj, prop, val) {
-				obj[prop].textContent = `${val}`;
-				return true;
+			obj[prop].textContent = `${val}`;
+			return true;
 		}
 	});
 };
-
-var TBy=" <span class='good'>[TB]</span>"; // u2713
-var TBn=" <span class='bad'>[TB]</span>"; // u2715
-var RFPy=" <span class='good'>[RFP]</span>";
-var RFPn=" <span class='bad'>[RFP]</span>";
-var FILEy=" <span class='ttc'>[file:]</span>"
-var TTC=" <span class='ttc'>test to come</span>"
-var amFF = false;
-if (isNaN(window.mozPaintCount) === false) {amFF = true};
 
 function showhide(toggleType, toggleID, togWord) {
 	var xyz = document.getElementsByClassName("tog"+toggleID); var abc;
