df5ce88c9d7a0d3ffdfd52d72b6381f611923e85
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-09 05:01:43 +0000

    Update media.js

--- js/media.js
+++ js/media.js
@@ -3,10 +3,19 @@
 'use strict';
 
 function outputMedia() {
-	// media.media-capabilities.enabled
-	if ("mediaCapabilities" in navigator) {dom.nMediaCapabilities="enabled"}
-		else {if (Symbol.for(`foo`).description == "foo"){dom.nMediaCapabilities="disabled"}
-			else {dom.nMediaCapabilities="not supported"};};
+
+	// media.media-capabilities.enabled : FF63+
+	if ("mediaCapabilities" in navigator) {
+		dom.nMediaCapabilities="enabled";
+	} else {
+		// FF63 feature test
+		if (Symbol.for(`foo`).description == "foo") {
+			dom.nMediaCapabilities="disabled";
+		} else {
+			dom.nMediaCapabilities="not supported";
+		};
+	};
+
 };
 
 outputMedia();
