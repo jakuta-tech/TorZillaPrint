85921e45e99d977159bcd51124037221c2124dd5
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-10 18:04:28 +0000

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var dom;
+var jsZoom;
 var TBy=" <span class='good'>[TB]</span>"; // u2713
 var TBn=" <span class='bad'>[TB]</span>"; // u2715
 var RFPy=" <span class='good'>[RFP]</span>";
@@ -10,3 +11,7 @@ var TTC=" <span class='ttc'>test to come</span>"
 var amFF = false;
 if (isNaN(window.mozPaintCount) === false) {amFF = true};
 
+/* other Firefox detection methods
+if (isNaN(window.mozInnerScreenX) === false) {"FF"};
+if (isNaN(window.window.scrollMaxX) === false) {"FF"};
+if (navigator.oscpu == undefined){} else {"FF"}; */
