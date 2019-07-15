808c64f1401d334aee0f4bec8582e69475e466f8
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-15 04:54:02 +0000

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -2,17 +2,24 @@
 
 var dom;
 var jsZoom;
-var TBy=" <span class='good'>[TB]</span>"; // u2713
-var TBn=" <span class='bad'>[TB]</span>"; // u2715
-var RFPy=" <span class='good'>[RFP]</span>";
-var RFPn=" <span class='bad'>[RFP]</span>";
-var FILEy=" <span class='ttc'>[file:]</span>"
-var TTC=" <span class='ttc'>test to come</span>"
-var CORS="<span class='ttc'>[file:] [Cross-Origin Request Blocked]</span>"
+
+var tor_browser_green = " <span class='good'>[TB]</span>"; // u2713
+var tor_browser_red = " <span class='bad'>[TB]</span>"; // u2715
+
+var rfp_green = " <span class='good'>[RFP]</span>";
+var rfp_red = " <span class='bad'>[RFP]</span>";
+
+var note_file = " <span class='ttc'>[file:]</span>"
+var note_testtocome = " <span class='ttc'>test to come</span>"
+
+var error_file_cors = "<span class='ttc'>[file:] [Cross-Origin Request Blocked]</span>"
+var error_file_xhr = "<span class='bad'>[test error: xhr]</span>";
+var error_iframe = "<span class='bad'>[test error: iframe]</span>";
+
 var amFF = false;
 if (isNaN(window.mozPaintCount) === false) {amFF = true};
 
 /* other Firefox detection methods
-if (isNaN(window.mozInnerScreenX) === false) {"FF"};
-if (isNaN(window.window.scrollMaxX) === false) {"FF"};
-if (navigator.oscpu == undefined){} else {"FF"}; */
+if (isNaN(window.mozInnerScreenX) === false) {"amFF = true"};
+if (isNaN(window.window.scrollMaxX) === false) {"amFF = true"};
+if (navigator.oscpu == undefined){} else {"amFF = true"}; */
