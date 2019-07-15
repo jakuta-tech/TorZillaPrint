5bc24d617ddff1b6d93dc2d504429881c14e680e
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-15 04:52:37 +0000

    Update domrect.js

--- js/domrect.js
+++ js/domrect.js
@@ -6,12 +6,10 @@
 	 https://canvasblocker.kkapsner.de/test/
 	 https://github.com/kkapsner/CanvasBlocker */
 
-(function(){
-	const iframeDR = document.getElementById("iframeDR");
+function outputDomRect() {
 	function getElements(){
-		const docDR = iframeDR.contentDocument;
-		//return Array.from(docDR.querySelectorAll("*[id^=rect]"));
-		return Array.from(docDR.querySelectorAll(".testRect"));
+		let doc = iframe.contentDocument;
+		return Array.from(doc.querySelectorAll(".testRect"));
 	}
 	function createTest(method, callback){
 	const properties = ["x", "y", "width", "height", "top", "left", "right", "bottom"];
@@ -27,13 +25,13 @@
 				crypto.subtle.digest("SHA-256", data).then(function(hash){
 					// FF append file:// notation
 					if ((location.protocol) == "file:") {
-						document.getElementById(method).innerHTML = byteArrayToHex(hash)+FILEy;
+						document.getElementById(method).innerHTML = byteArrayToHex(hash) + note_file;
 					} else {
 						document.getElementById(method).innerHTML = byteArrayToHex(hash);
 					}
 				});
 				// output results
-				var item=0;
+				let item=0;
 				properties.map(function(property){
 					return rects.map(function(rect, i){
 						item=item+1;
@@ -43,31 +41,35 @@
 				}).join("")
 		}
 		performTest();
-	}
-	// iframe-load & button-eventListener
-	function drTest() {
-		// clear data regardless of auto-run or re-run
-		var drA = 1;
-		while (drA < 5) {
-			document.getElementById("dr"+drA).innerHTML = "&nbsp"; var drB = 1;
-			while (drB < 49) {document.getElementById("dr"+drA+drB).innerHTML = "&nbsp"; drB++;}
-			drA++;
-		};
-		if (amFF == false && (location.protocol) == "file:") {
-			// output file:/// for non FF
-			dom.dr1.innerHTML=FILEy; dom.dr2.innerHTML=FILEy; dom.dr3.innerHTML=FILEy; dom.dr4.innerHTML=FILEy; 
-		} else {
+	};
+
+	// clear detailed data
+	for (let i = 1; i < 5; i++) {
+		document.getElementById("dr"+i).innerHTML = "&nbsp";
+		for (let j = 1; j < 49; j++) {
+			document.getElementById("dr"+i+j).innerHTML = "&nbsp";
+		}
+	};
+
+	// set iframe source
+	let iframe = document.getElementById("iframeDR");
+	iframe.src = "iframes/domrect.html";
+	iframe.addEventListener("load", function(){
+		// catch iframe error
+		try {
+			let testerror = iframe.contentWindow.document.getElementById("rect1");
+
 			setTimeout(function(){
 				// run the four tests
 				createTest("dr1", function(element){return element.getClientRects()[0];});
 				createTest("dr2", function(element){return element.getBoundingClientRect();});
 				createTest("dr3", function(element){
-					var range = document.createRange();
+					let range = document.createRange();
 					range.selectNode(element);
 					return range.getClientRects()[0];
 				});
 				createTest("dr4", function(element){
-					var range = document.createRange();
+					let range = document.createRange();
 					range.selectNode(element);
 					return range.getBoundingClientRect();
 				});
@@ -79,14 +81,25 @@
 					};
 				}, 50); // delay to make sure things are loaded
 			}, 120); // artifical delay to show clearing and stop jitter
-		}
-	};
-	// set the iframe source here
-	iframeDR.src = "iframes/domrect.html";
-	// listen for it
-	iframeDR.addEventListener("load", function(){
-		drTest();
-		// add event listener to rerun button
-		document.getElementById("drRun").addEventListener("click", drTest);
+
+		} catch(e) {
+			// iframe didn't load
+			if ((location.protocol) == "file:") {
+				// file: Cross-Origin Request Blocked
+				dom.dr1.innerHTML = error_file_cors;
+				dom.dr2.innerHTML = error_file_cors;
+				dom.dr3.innerHTML = error_file_cors;
+				dom.dr4.innerHTML = error_file_cors;
+			} else {
+				// iframe is blocked
+				dom.dr1.innerHTML = error_iframe;
+				dom.dr2.innerHTML = error_iframe;
+				dom.dr3.innerHTML = error_iframe;
+				dom.dr4.innerHTML = error_iframe;
+			};
+		};
 	});
-}());
+
+};
+
+outputDomRect();
