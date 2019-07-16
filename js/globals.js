76fbb56d0a7702b3ca995b3945160856c9cbcf13
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-16 08:37:22 +0000

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -5,17 +5,22 @@ var jsZoom;
 
 var tor_browser_green = " <span class='good'>[TB]</span>"; // u2713
 var tor_browser_red = " <span class='bad'>[TB]</span>"; // u2715
-
 var rfp_green = " <span class='good'>[RFP]</span>";
 var rfp_red = " <span class='bad'>[RFP]</span>";
 
+// notation when consecutive tests detect noise / randomization
+var note_random = " <span class='good'>[random]</span>"
+
+// notation for developer
 var note_file = " <span class='ttc'>[file:]</span>"
 var note_testtocome = " <span class='ttc'>test to come</span>"
 
+// generic errors so we can output something for the end user
 var error_file_cors = "<span class='ttc'>[file:] [Cross-Origin Request Blocked]</span>"
 var error_file_xhr = "<span class='bad'>[test error: xhr]</span>";
 var error_iframe = "<span class='bad'>[test error: iframe]</span>";
 
+// sometimes we only want to test or output for Firefox
 var amFF = false;
 if (isNaN(window.mozPaintCount) === false) {amFF = true};
 
