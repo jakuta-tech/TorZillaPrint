fb5769b798f476998498c9169a1e9a555225893f
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-16 17:12:01 +0000

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -12,11 +12,11 @@ var rfp_red = " <span class='bad'>[RFP]</span>";
 var note_random = " <span class='good'>[random]</span>"
 
 // notation for developer
-var note_file = " <span class='ttc'>[file:]</span>"
-var note_testtocome = " <span class='ttc'>test to come</span>"
+var note_file = " <span class='neutral'>[file:]</span>"
+var note_testtocome = " <span class='neutral'>test to come</span>"
 
 // generic errors so we can output something for the end user
-var error_file_cors = "<span class='ttc'>[file:] [Cross-Origin Request Blocked]</span>"
+var error_file_cors = "<span class='neutral'>[file:] [Cross-Origin Request Blocked]</span>"
 var error_file_xhr = "<span class='bad'>[test error: xhr]</span>";
 var error_iframe = "<span class='bad'>[test error: iframe]</span>";
 
