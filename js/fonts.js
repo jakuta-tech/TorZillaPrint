68dba3e20994ff118453315cbc8406c843aa63a9
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-07-27 03:31:03 +0000

    Update fonts.js

--- js/fonts.js
+++ js/fonts.js
@@ -221,6 +221,9 @@ function output_enumerate_fallback(type){
 		fontFBTest.style.wordSpacing = "normal"
 
 		fontFBTest.style.fontFamily = fontFamily;
+
+		console.log (fontFamily, fontFBTest.offsetWidth);
+
 		return fontFBTest.offsetWidth;
 	};
 
