f593b7f62e1c6eb4d8c6cafea83088e5827facd7
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-08-09 15:19:28 +0000

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -2,6 +2,14 @@
 
 "use strict";
 
+/* code based on work by
+	kkapsner and canvasblocker
+	- https://canvasblocker.kkapsner.de/test/
+	- https://github.com/kkapsner/CanvasBlocker
+	openWPM
+	- https://audiofingerprint.openwpm.com/
+*/
+
 function reset_audio() {
 	// change font color: try not to shrink/grow elements
 	dom.audio1data.style.color = "#1a1a1a";
@@ -9,55 +17,8 @@ function reset_audio() {
 	dom.audio3data.style.color = "#1a1a1a";
 }
 
-/* code based on work by kkapsner and canvasblocker
-	https://canvasblocker.kkapsner.de/test/
-	https://github.com/kkapsner/CanvasBlocker
-*/
-function run_pxi_fp(){
-	let context = new window.OfflineAudioContext(1, 44100, 44100);
-	// create oscillator
-	let pxi_oscillator = context.createOscillator();
-	pxi_oscillator.type = "triangle";
-	pxi_oscillator.frequency.value = 1e4;
-	// create and configure compressor
-	let pxi_compressor = context.createDynamicsCompressor();
-	pxi_compressor.threshold && (pxi_compressor.threshold.value = -50);
-	pxi_compressor.knee && (pxi_compressor.knee.value = 40);
-	pxi_compressor.ratio && (pxi_compressor.ratio.value = 12);
-	pxi_compressor.reduction && (pxi_compressor.reduction.value = -20);
-	pxi_compressor.attack && (pxi_compressor.attack.value = 0);
-	pxi_compressor.release && (pxi_compressor.release.value = .25);
-	// connect nodes
-	pxi_oscillator.connect(pxi_compressor);
-	pxi_compressor.connect(context.destination);
-	// start audio processing
-	pxi_oscillator.start(0);
-	context.startRendering();
-	context.oncomplete = function(event) {
-		let str = "";
-		let copyTest = new Float32Array(44100);
-		event.renderedBuffer.copyFromChannel(copyTest, 0);
-		let getTest = event.renderedBuffer.getChannelData(0);
-		Promise.all([
-			crypto.subtle.digest("SHA-256", getTest),
-			crypto.subtle.digest("SHA-256", copyTest),
-			]).then(function(hashes){
-				dom.audioCopy = byteArrayToHex(hashes[0]);
-				dom.audioGet = byteArrayToHex(hashes[1]);
-			});
-			let sum = 0;
-			for (let i = 4500; i < 5000; i++) {
-				sum += Math.abs(getTest[i]);
-			}
-			dom.audioSum = sum;
-			pxi_compressor.disconnect();
-	};
-};
-
-/* code based on work by openWPM
-	https://audiofingerprint.openwpm.com/
-*/
-function run_nt_vc_fp() {
+function get_nt_vc() {
+	let t0 = performance.now();
 	let obj;
 	let hash = "", // str to hash
 		output = "", // pretty output
@@ -108,17 +69,65 @@ function run_nt_vc_fp() {
 	}
 	// reset color
 	dom.audio1data.style.color = "#b3b3b3";
+	let t1 = performance.now();
+	if (mPerf) {console.debug("audio context: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
+};
+
+function get_pxi(){
+	let t0 = performance.now();
+
+	let context = new window.OfflineAudioContext(1, 44100, 44100);
+	// create oscillator
+	let pxi_oscillator = context.createOscillator();
+	pxi_oscillator.type = "triangle";
+	pxi_oscillator.frequency.value = 1e4;
+	// create and configure compressor
+	let pxi_compressor = context.createDynamicsCompressor();
+	pxi_compressor.threshold && (pxi_compressor.threshold.value = -50);
+	pxi_compressor.knee && (pxi_compressor.knee.value = 40);
+	pxi_compressor.ratio && (pxi_compressor.ratio.value = 12);
+	pxi_compressor.reduction && (pxi_compressor.reduction.value = -20);
+	pxi_compressor.attack && (pxi_compressor.attack.value = 0);
+	pxi_compressor.release && (pxi_compressor.release.value = .25);
+	// connect nodes
+	pxi_oscillator.connect(pxi_compressor);
+	pxi_compressor.connect(context.destination);
+	// start audio processing
+	pxi_oscillator.start(0);
+	context.startRendering();
+	context.oncomplete = function(event) {
+		let str = "";
+		let copyTest = new Float32Array(44100);
+		event.renderedBuffer.copyFromChannel(copyTest, 0);
+		let getTest = event.renderedBuffer.getChannelData(0);
+		Promise.all([
+			crypto.subtle.digest("SHA-256", getTest),
+			crypto.subtle.digest("SHA-256", copyTest),
+			]).then(function(hashes){
+				dom.audioCopy = byteArrayToHex(hashes[0]);
+				dom.audioGet = byteArrayToHex(hashes[1]);
+			});
+			let sum = 0;
+			for (let i = 4500; i < 5000; i++) {
+				sum += Math.abs(getTest[i]);
+			}
+			dom.audioSum = sum;
+			pxi_compressor.disconnect();
+			let t1 = performance.now();
+			if (mPerf) {console.debug("audio pxi: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
+	};
 };
 
 function outputAudio() {
+
 	// detect web audio API enabled
 	try {
-		let contextC = new AudioContext();
+		let contextA = new AudioContext();
 		dom.audioSupport = "enabled";
 		// from openWPM: there may be weird interference effects if the
-		// prints are run sequentially with no delay, hence the sleeping.
-		run_pxi_fp();
-		setTimeout(function() { run_nt_vc_fp(); }, 500);
+		// prints are run sequentially with no delay, hence the sleeping
+		get_nt_vc();
+		setTimeout(function() { get_pxi(); }, 50);
 	}	catch(e) {
 		dom.audioSupport = "disabled";
 		dom.audioCopy = "n/a";
