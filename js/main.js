ecb2568f74152cb94a8a15e996f85c1b744a5ddd
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-08-09 15:27:39 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 'use strict';
 dom = getUniqueElements();
+
+gt0 = performance.now();
+let t1 = performance.now();
+if (sPerf) {console.debug("  ** section start timing: main.js loaded: " + (t1 - gt0) + " ms")};
