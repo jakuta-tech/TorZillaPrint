9fe666802f63a428a2e114f9661ccb7bf8a5c225
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-08-10 07:28:52 +0000

    Update webgl.js

--- js/webgl.js
+++ js/webgl.js
@@ -2,35 +2,40 @@
 
 'use strict';
 
-function outputWebGL() {
+function get_webgl_basics() {
 	let t0 = performance.now();
-
 	function getWebglContext(canvas){
 		try { 
 			return canvas.getContext("webgl") || canvas.getContext("experimental-webgl");    
 		} catch(e) {}
 		return null;
-	}
-
+	};
 	let canvas = document.createElement('canvas');
 	let gl = getWebglContext(canvas);
 	if (gl) {
 		let ext = gl.getExtension('WEBGL_debug_renderer_info');
 		if(ext) {
-
 			//console.log ( "WebGL Version: " + gl.getParameter(gl.VERSION) ); 
 			//console.log ( "Shading Language Version: " + gl.getParameter(gl.SHADING_LANGUAGE_VERSION) );
 			//console.log ( "WebGL Vendor: " + gl.getParameter(gl.VENDOR) );
 			//console.log ( "WebGL Renderer: " + gl.getParameter(gl.RENDERER) );
 			//console.log ( "WebGL Unmasked Vendor: " + gl.getParameter(ext.UNMASKED_VENDOR_WEBGL) );
 			//console.log ( "WebGL Unmasked Renderer: " + gl.getParameter(ext.UNMASKED_RENDERER_WEBGL) );
-
 		}
 	}
+	// perf
+	let t1 = performance.now();
+	if (mPerf) {console.debug("  ** webgl basics: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
+};
+
+
+function outputWebGL() {
+	let t0 = performance.now();
+	// functions
 
 	// perf
 	let t1 = performance.now();
-	if (sPerf) {console.debug("webgl section: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
+	if (sPerf) {console.debug("  ** section webgl: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
 };
 
 outputWebGL();
