63493a02d14045dac4fbbdef437f4c0bc209845b
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-08-15 13:42:05 +0000

    Update language.js

--- js/language.js
+++ js/language.js
@@ -2,111 +2,108 @@
 
 'use strict';
 
-function get_app_language() {
-	if (isFirefox == true) {
+var t0lang;
+var iframeBlocked = false;
+var dtd2 = "";
 
-		// are images or iframes blocked
-		let iframeBlocked = false;
-		let imageTest = document.getElementById("APP_LANG_IMAGE");
-		imageTest.src="images/dummy.png"; // 1px high
-		setTimeout(function(){
-			// empty_src=0, broken_src=24 (more or less: enough to be seen)
-			// extensions blocking = if placeholders = 20 (more or less)
-			// but if it's 1px then the image was loaded
-			// console.log("image test is :" + imageTest.offsetHeight);
-			if (imageTest.offsetHeight !== 1) {
-				dom.appLang4.innerHTML = error_image
-			} else {
-				// MediaDocument.properties
-				let iframe3 = document.getElementById("APP_LANG_4");
-				iframe3.src="images/dummy.png";
-				iframe3.addEventListener("load", function() {
-					try {
-						dom.appLang4 = (this.contentWindow.document.title);
-					} catch(e) {
-						if ((location.protocol) == "file:") {
-							// file: Cross-Origin Request Blocked
-							dom.appLang4.innerHTML = error_file_cors
-						} else {
-							// iframe blocked
-							dom.appLang4.innerHTML = error_iframe;
-							iframeBlocked = true;
-						};
-					};
-					// rest the src to empty
-					imageTest.src="";
-				});
-			}
-		}, 1500); // lots of time for Tor Browser
-
-		// dom.properties
-		dom.appLang1 = document.getElementById("APP_LANG_1").validationMessage;
-
-		// dtd
-		let dtd1 = "", dtd1error = false;
-		let iframe1 = document.getElementById("APP_LANG_2");
-		iframe1.src="iframes/dtdlocale.xml";
-		iframe1.addEventListener("load", dtdlocale1)
-		function dtdlocale1() {
-			try {
-				dtd1 = iframe1.contentDocument.getElementById("DTD1").innerText;
-				window.removeEventListener("load", dtdlocale1)
-			} catch(e) {
-				if ((location.protocol) == "file:") {
-					// file: Cross-Origin Request Blocked
-					dtd1 = error_file_cors
-				} else {
-					// cannot tell if iframe blocked or bugzilla patch is working
-					dtd1error = true;
-					dtd1 = error_iframe;
-				};
-			};
-		};
-
-		// dtd nullprinciple
-		let dtd2 = "";
-		let iframe2 = document.getElementById("APP_LANG_3");
-		iframe2.src="data:application/xml;charset=utf-8,%3C%21DOCTYPE%20html%20SYSTEM%20%22chrome%3A%2F%2Fglobal%2Flocale%2FnetError.dtd%22%3E%3Chtml%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxhtml%22%3E%3Chead%3E%3Cmeta%20charset%3D%22utf-8%22%2F%3E%0D%0A%20%20%3C%2Fhead%3E%0D%0A%20%20%3Cbody%3E%3Cspan%20id%3D%22text-container%22%3E%26loadError.label%3B%3C%2Fspan%3E%0D%0A%20%20%3Cscript%3E%0D%0A%20%20window.addEventListener%28%27message%27%2C%20%28e%29%20%3D%3E%20%7B%0D%0A%20%20%20%20e.source.postMessage%28document.getElementById%28%27text-container%27%29.innerText%2C%20%27%2A%27%29%3B%0D%0A%20%20%7D%29%3B%0D%0A%20%20%3C%2Fscript%3E%0D%0A%20%20%3C%2Fbody%3E%0D%0A%3C%2Fhtml%3E";
-		iframe2.addEventListener("load", dtdlocale2)
-		function dtdlocale2() {
-			window.addEventListener('message', ({ data }) => dtd2 = data);
-			document.getElementById("APP_LANG_3").contentWindow.postMessage('foo', '*');
-		};
-
-		// output dtds: wait to make sure we received the dtd1 message
-		setTimeout(function(){
-			// dtd1
-			if (dtd1error == true) {
-				// genuine test error e.g iframe being blocked or the patch working
-				if (dtd2 == "") {
-					// if dtd2 is blank that's a good sign
-					if (iframeBlocked == false) {
-						// if iframe from mediadocument was not blocked that's a good sign
-				    dtd1 = "<span class='good'>[bugzilla 467035]</span>"
-					}
-				}
-			};
-			dom.appLang2.innerHTML = dtd1;
-			// dtd2
-			if (dtd2 == "") {
-				dom.appLang3.innerHTML = "<span class='good'>[bugzilla 467035]</span>";
-			} else {
-				dom.appLang3.innerHTML = dtd2;
-			}
-		}, 1800);
-
-		// xmlparser.properties
-		let appL5doc = (new DOMParser).parseFromString('getyourlocale', 'application/xhtml+xml');
-		let str2 = (appL5doc.getElementsByTagName('parsererror')[0].firstChild.textContent);
-		// shorten output by removing location URL
-		let nb1 = str2.search("http");
-		if (nb1 == -1) {
-			nb1 = str2.search("file")
+function get_app_lang_dtd2() {
+	// dtd nullprinciple
+	dtd2 = "";
+	let iframe = document.getElementById("appLang_3");
+	iframe.src="data:application/xml;charset=utf-8,%3C%21DOCTYPE%20html%20SYSTEM%20%22chrome%3A%2F%2Fglobal%2Flocale%2FnetError.dtd%22%3E%3Chtml%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxhtml%22%3E%3Chead%3E%3Cmeta%20charset%3D%22utf-8%22%2F%3E%0D%0A%20%20%3C%2Fhead%3E%0D%0A%20%20%3Cbody%3E%3Cspan%20id%3D%22text-container%22%3E%26loadError.label%3B%3C%2Fspan%3E%0D%0A%20%20%3Cscript%3E%0D%0A%20%20window.addEventListener%28%27message%27%2C%20%28e%29%20%3D%3E%20%7B%0D%0A%20%20%20%20e.source.postMessage%28document.getElementById%28%27text-container%27%29.innerText%2C%20%27%2A%27%29%3B%0D%0A%20%20%7D%29%3B%0D%0A%20%20%3C%2Fscript%3E%0D%0A%20%20%3C%2Fbody%3E%0D%0A%3C%2Fhtml%3E";
+	iframe.addEventListener("load", dtdlocale2)
+	function dtdlocale2() {
+		window.addEventListener('message', ({ data }) => dtd2 = data);
+		document.getElementById("appLang_3").contentWindow.postMessage('foo', '*');
+		if (dtd2 == "") {
+			dom.appLang3.innerHTML = "<span class='good'>[bugzilla 467035]</span>";
+		} else {
+			dom.appLang3.innerHTML = dtd2;
 		}
-		let nb2 = str2.search("html") + 4;
-		dom.appLang5 = str2.slice(0,nb1-1) + str2.slice(nb2);
-
 	};
+};
+
+function get_app_lang_mediadocument() {
+	let t0 = performance.now();
+	// MediaDocument.properties
+	
+	let iframe = document.getElementById("appLang_4");
+	function output_mediadocument(string) {
+		dom.appLang4.innerHTML = string;
+		// perf
+		let t1 = performance.now();
+		if (mPerf) {console.debug("app language MediaDocument: " + (t1-t0lang) + " ms" + " | " + (t1 - gt0) + " ms")};
+	};
+	function run_mediadocument() {
+		try {
+			//let answer = (this.contentWindow.document.title);
+			let output = (iframe.contentWindow.document.title);
+			output_mediadocument(output);
+		} catch(e) {
+			if ((location.protocol) == "file:") {
+				// file: Cross-Origin Request Blocked
+				output_mediadocument(error_file_cors);
+			} else {
+			// iframe blocked
+				output_mediadocument(error_iframe);
+			};
+		};
+	};
+	function check_mediadocument() {
+		let iframe = document.getElementById("appLang_4");
+		// load the iframe
+		if (iframe.src == "") {
+			iframe.src="images/dummy.png";
+			iframe.addEventListener("load", function(){
+				run_mediadocument();
+			});
+		} else {
+			// already loaded
+			run_mediadocument();
+		}
+	};
+
+	// keep checking if image is loaded, but stop after x tries
+	let image = document.getElementById("imageTest");
+	image.src="images/dummy.png"; // 1px high
+	let counter = 0;
+	function check_image() {
+		if (counter < 40) {
+			if (image.offsetHeight == 1) {
+				// empty_src=0px, broken_src= approx 24px
+				// extensions blocking images = if placeholders = approx 20px
+				// therefore: if 1px then our image was loaded
+				clearInterval(checking);
+				image.src=""; // reset src
+				check_mediadocument();
+			}
+		} else {
+			clearInterval(checking);
+			output_mediadocument(error_image);
+			image.src=""; // reset src
+		}
+		counter++;
+	}
+	let checking = setInterval(check_image, 50)
+};
+
+function get_app_lang_xmlparser() {
+	let doc = (new DOMParser).parseFromString('getyourlocale', 'application/xhtml+xml');
+	let str = (doc.getElementsByTagName('parsererror')[0].firstChild.textContent);
+	// remove location URL
+	let start = str.search("http");
+	if (start == -1) { start = str.search("file")};
+	let end = str.search("html") + 4;
+	let output = str.slice(0,start-1) + str.slice(end);
+	// remove anchor
+	start = output.search("#");
+	if (start !== -1) {
+		let strTemp = output.substring(start, output.length);
+		strTemp = strTemp.replace(/(?:\r|\n).*$/, ' ');
+		end = strTemp.search(" ");
+		output = output.slice(0,start) + output.slice(start+end,output.length);
+	};
+	dom.appLang5 = output;	
 };
 
 function outputLanguage() {
@@ -193,8 +190,17 @@ function outputLanguage() {
 	let t1 = performance.now();
 	if (sPerf) {console.debug("  ** section language [excl app lang]: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
 
-	// start app lang pocs
-	get_app_language();
+	// app lang pocs: FF only
+	if (isFirefox == true) {
+		t0lang = performance.now();
+		dom.appLang1 = document.getElementById("appLang_1").validationMessage;
+		get_app_lang_xmlparser();
+		get_app_lang_dtd2();
+		get_app_lang_mediadocument();
+		//test_iframe();
+		//get_app_lang_dtd1();
+	};
+
 };
 
 outputLanguage();
