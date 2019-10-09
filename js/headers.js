aaea4fd12239d913cbcf4dace5892194537beb3c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-10-09 03:04:29 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -10,7 +10,12 @@ function outputHeaders() {
 	// network info api: dom.netinfo.enabled
 	if ("connection" in navigator) {
 		dom.nNetwork = "enabled";
-		dom.nConnection = navigator.connection.type;
+		let strTemp = navigator.connection.type
+		if (strTemp == "unknown") {
+			dom.nConnection.innerHTML = strTemp + rfp_green;
+		} else {
+			dom.nConnection.innerHTML = strTemp + rfp_red;
+		}
 	} else {
 		dom.nNetwork = "disabled";
 		dom.nConnection = navigator.connection;
