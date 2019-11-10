77b8cf9d506beab57ba746c31c6a96feaba93439
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-11-10 14:31:16 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -4,22 +4,20 @@
 
 function outputHeaders() {
 	let t0 = performance.now();
-	// dnt
+
 	dom.nDoNotTrack = navigator.doNotTrack;
 	dom.nOnLine = navigator.onLine;
+
 	// network info api: dom.netinfo.enabled
 	if ("connection" in navigator) {
 		dom.nNetwork = "enabled";
-		let strTemp = navigator.connection.type
-		if (strTemp == "unknown") {
-			dom.nConnection.innerHTML = strTemp + rfp_green;
-		} else {
-			dom.nConnection.innerHTML = strTemp + rfp_red;
-		}
+		let s = navigator.connection.type;
+		dom.nConnection.innerHTML = (s == "unknown" ? s + rfp_green : s + rfp_red)
 	} else {
 		dom.nNetwork = "disabled";
 		dom.nConnection = navigator.connection;
 	};
+
 	// perf
 	let t1 = performance.now();
 	if (sPerf) {console.debug("  ** section headers: " + (t1-t0) + " ms" + " | " + (t1 - gt0) + " ms")};
