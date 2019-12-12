680b4f2f302475f5173098d941a13dfec5a2d683
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-12-12 04:14:33 +0000

    Update css.js

--- js/css.js
+++ js/css.js
@@ -21,28 +21,49 @@ function get_prefers_reduced_motion() {
 	dom.pReducedMotion.innerHTML = k;
 };
 
-function get_system_colors() {
+function get_system_colors(type) {
 	let s = "",
+		array = [],
+		e = dom.sColorElement;
+	if (type == "system") {
 		array = ['ActiveBorder','ActiveCaption','AppWorkspace','Background','ButtonFace',
-	'ButtonHighlight','ButtonShadow','ButtonText','CaptionText','GrayText','Highlight',
-	'HighlightText','InactiveBorder','InactiveCaption', 'InactiveCaptionText','InfoBackground',
-	'InfoText','Menu','MenuText','Scrollbar','ThreeDDarkShadow','ThreeDFace', 'ThreeDHighlight',
-	'ThreeDLightShadow','ThreeDShadow','Window','WindowFrame', 'WindowText'];
-	let e = dom.sColorElement;
+		'ButtonHighlight','ButtonShadow','ButtonText','CaptionText','GrayText','Highlight',
+		'HighlightText','InactiveBorder','InactiveCaption', 'InactiveCaptionText','InfoBackground',
+		'InfoText','Menu','MenuText','Scrollbar','ThreeDDarkShadow','ThreeDFace', 'ThreeDHighlight',
+		'ThreeDLightShadow','ThreeDShadow','Window','WindowFrame', 'WindowText'];
+	} else if (type == "moz") {
+		array = ['-moz-ButtonDefault','-moz-ButtonHoverFace','-moz-CellHighlight','-moz-CellHighlightText',
+		'-moz-Combobox','-moz-ComboboxText','-moz-Dialog','-moz-DialogText','-moz-DragTargetZone',
+		'-moz-EvenTreeRow','-moz-Field','-moz-FieldText','-moz-MenuHover','-moz-MenuHoverText','-moz-MenubarText',
+		'-moz-NativeHyperlinkText','-moz-OddTreeRow','-moz-html-CellHighlight','-moz-html-CellHighlightText',
+		'-moz-mac-chrome-active','-moz-mac-chrome-inactive','-moz-mac-focusring','-moz-mac-menuselect',
+		'-moz-mac-menushadow','-moz-mac-menutextdisable','-moz-mac-menutextselect','-moz-mac-menutextselect',
+		'-moz-mac-DisabledToolbarText','-moz-mac-AlternatePrimaryHighlight','-moz-mac-SecondaryHighlight',
+		'-moz-win-MediaText','-moz-win-CommunicationsText','-moz-ActiveHyperlinkText','-moz-HyperLinkText',
+		'-moz-VisitedHyperlinkText','-moz-default-background-color','-moz-default-color'];
+	}
 	array.forEach(function (item) {
 		e.style.backgroundColor = item;
 		s = s + window.getComputedStyle(e, null).getPropertyValue("background-color")
+		// console.log(window.getComputedStyle(e, null).getPropertyValue("background-color"), item)
 	});
 	s = sha1(s);
-	dom.sColorHash.innerHTML = (s == "b5e2344c265fc498d2fb8e0f84951e8d501ad481" ? s + rfp_green : s + rfp_red);
+	if (type == "system") {
+		dom.sColorHash.innerHTML = (s == "b5e2344c265fc498d2fb8e0f84951e8d501ad481" ? s + rfp_green : s + rfp_red);
+	} else if (type == "moz") {
+		// ad55ec68f093cb2869190bc1cd18b206424db02b = all rgb(0, 0, 0)
+		dom.mColorHash.innerHTML = s
+	}
 };
+
 
 function outputCSS() {
 	let t0 = performance.now();
 	// functions
 	get_prefers_color_scheme();
 	get_prefers_reduced_motion();
-	get_system_colors();
+	get_system_colors("system");
+	get_system_colors("moz");
 	// perf
 	let t1 = performance.now();
 	if (sPerf) {outputDebug("1", "css", (t1-t0), (t1 - gt0))};
