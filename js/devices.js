5539417213ef6acfecbf898063fb28c6759defda
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-12-26 13:34:35 +0000

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -2,76 +2,215 @@
 
 'use strict';
 
-function get_touch_event() {
-	let result = false;
-	try {
-		document.createEvent("TouchEvent");
-		result = true
-	} catch (e) {}
-  return result
+function reset_devices() {
+	// hide w/ color: don't shrink elements
+	dom.mimeTypes.style.color = zhide;
+	dom.plugins.style.color = zhide;
+	dom.eMediaDevices.style.color = zhide;
+};
+
+function get_gamepads() {
+	// gamepads
+	if ("getGamepads" in navigator) {
+		dom.nGetGamepads = "enabled";
+		// ToDo: gamepads: enumerate
+		dom.eGamepads.innerHTML = note_ttc
+	} else {
+		dom.nGetGamepads = "disabled";
+		dom.eGamepads = "n/a";
+	}
 }
 
-function outputDevices() {
-	let t0 = performance.now();
-	let e = "enabled", d = "disabled", ns = "not supported", na = "n/a";
-
-	// hardwareConcurrency
-	let str = "";
-	str = navigator.hardwareConcurrency;
-	dom.nHardwareConcurrency.innerHTML = (str == "2" ? str + rfp_green : str + rfp_red);
-
-	// gamepads
-	dom.nGetGamepads = (navigator.getGamepads ? e: d);
-
+function get_media_devices() {
 	// media.navigator.enabled
 	if ("mediaDevices" in navigator) {
-		dom.nMediaDevices = e
-		// enumerate media devices
-		try {
-			dom.eMediaDevices.innerHTML = note_testtocome
-		} catch(e) {
-			dom.eMediaDevices = "no: " + e.name
-		}
+		dom.nMediaDevices = "enabled";
+		// enumerate
+		let str = "", pad = 0, strPad = "";
+		navigator.mediaDevices.enumerateDevices().then(function(devices) {
+			devices.forEach(function(device) {
+				pad = device.kind.length + 2;
+				str += device.kind + ": " + device.deviceId
+				if (device.groupId.length > 0) {
+					strPad = "group: "; strPad = strPad.padStart(pad)
+					str += "<br>" + strPad + device.groupId
+				}
+				if (device.label.length > 0) {
+					strPad = "label: "; strPad = strPad.padStart(pad)
+					str += "<br>" + strPad + device.label
+				}
+				str += "<br>"
+			});
+			dom.eMediaDevices.innerHTML = str
+		})
+		.catch(function(err) {
+			console.debug("enumerateDevices", err.name, err.message);
+		});
 	}	else {
-		dom.nMediaDevices = d;
-		dom.eMediaDevices = na;
-	};
+		dom.nMediaDevices = "disabled";
+		dom.eMediaDevices = "n/a";
+	}
+	dom.eMediaDevices.style.color = zshow;
+};
 
-	// dom.vr.enabled
-	if ("getVRDisplays" in navigator) {
-		dom.nGetVR = e;
-		// active VR displays
-		if ("activeVRDisplays" in navigator) {
-			if (navigator.activeVRDisplays == "") {
-				dom.nActiveVR = "[empty array]"
-			} else {
-				dom.nActiveVR = navigator.activeVRDisplays
+function get_mimetypes() {
+	if ("mimeTypes" in navigator) {
+		let mimes = navigator.mimeTypes
+		if (mimes.length == 0) {
+			dom.mimeTypes.innerHTML = "none"
+		} else {
+			let str = "";
+			for (let i = 0; i < mimes.length; i++) {
+				str += mimes[i].type
+				if (mimes[i].description != "") {str += ": " + mimes[i].type}
+				if (mimes[i].suffixes != "") {str += ": " + mimes[i].suffixes}
+				str += "<br>";
 			}
+			dom.mimeTypes.innerHTML = str
 		}
-	}	else {
-		dom.nGetVR = d;
-		dom.nActiveVR = na;
-	};
+	} else {
+		dom.mimeTypes = "disabled";
+	}
+	dom.mimeTypes.style.color = zshow;
+};
 
+function get_mm_pointer(type){
+	let x = "not supported",
+		f="fine", c="coarse", n="none",
+		q = "("+type+": ";
+	if (window.matchMedia(q+n+")").matches) x = "none";
+	if (window.matchMedia(q+c+")").matches) x = "coarse";
+	if (window.matchMedia(q+f+")").matches) x = "fine";
+	return x
+};
+
+function get_plugins() {
+	if ("plugins" in navigator) {
+		if (navigator.plugins.length == 0) {
+			dom.plugins.innerHTML = "none";
+		} else {
+			let str = "";
+			for (let i = 0; i < navigator.plugins.length; i++) {
+				str += navigator.plugins[i].name
+				if (navigator.plugins[i].filename == "") {
+					str += ": - : "
+				} else {
+					str += ": " + navigator.plugins[i].filename
+				}
+				if (navigator.plugins[i].description == "") {
+					str += ": - : "
+				} else {
+					str += ": " + navigator.plugins[i].description
+				}
+				str += "<br>"
+			}
+			dom.plugins.innerHTML = str
+		}
+	} else {
+		dom.plugins = "disabled";
+	}
+	dom.plugins.style.color = zshow;
+}
+
+function get_speech_synth() {
 	// media.webspeech.synth.enabled
 	if ("speechSynthesis" in window) {
-		dom.speechSynth = e;
+		dom.speechSynth = "enabled";
 		// speech engines
-		dom.speechEngines.innerHTML = note_testtocome;
+		function populateVoiceList() {
+			if(typeof speechSynthesis === 'undefined') {
+				return;
+			}
+			let voices = speechSynthesis.getVoices();
+			let str = "";
+			for (let i = 0; i < voices.length ; i++) {
+				str += voices[i].name + " [" + voices[i].lang + "]";
+				if (voices[i].default) { str += " : default" }
+				str += "<br>"
+			}
+			if (str == "") {
+				str = "none" + rfp_green; // RFP: 1333641
+			};
+			dom.speechEngines.innerHTML = str
+		}
+		populateVoiceList();
+		if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+			speechSynthesis.onvoiceschanged = populateVoiceList;
+		}
 	} else {
-		dom.speechSynth = d;
-		dom.speechEngines = na;
-	};
+		dom.speechSynth = "disabled";
+		dom.speechEngines = "n/a";
+	}
+};
 
+function get_speech_rec() {
 	// media.webspeech.recognition.enable
 	// NOTE: media.webspeech.test.enable until landed
 	try {
 		let recognition = new SpeechRecognition();
-		dom.speechRec = e;
+		dom.speechRec = "enabled";
 	} catch(e) {
 		// undefined
-		dom.speechRec = d + " [or " + ns + "]"
+		// ToDo: speech recognition: can I detect disabled vs not supported
+		dom.speechRec = "disabled [or not supported]"
 	}
+};
+
+function get_touch_event() {
+	// ToDo: TouchEvent: do i remove it on fail?
+	try {
+		document.createEvent("TouchEvent");
+		return true
+	} catch (e) {
+		return false
+	}
+};
+
+function get_vr() {
+	// dom.vr.enabled
+	if ("getVRDisplays" in navigator) {
+		dom.nGetVR = "enabled";
+		if ("activeVRDisplays" in navigator) {
+			let displays = navigator.activeVRDisplays;
+			if (displays.length == 0) {
+				// no active VR displays
+				dom.nActiveVR = "none"
+			} else {
+				// ToDo: activeVRDisplays: enumerate
+				dom.nActiveVR.innerHTML = note_ttc + " [" + displays.length + " items]";
+				for (let vc = 0; vc < displays.length; vc++) {
+					// console.debug(displays[i].displayId);
+				}
+			}
+		}
+	}	else {
+		dom.nGetVR = "disabled";
+		dom.nActiveVR = "n/a";
+	}
+};
+
+function outputDevices() {
+	let t0 = performance.now();
+
+	get_gamepads();
+	get_media_devices();
+	get_mimetypes();
+	get_plugins();
+	get_speech_synth();
+	get_speech_rec();
+	get_vr();
+
+	// hardwareConcurrency
+	if ("hardwareConcurrency" in navigator) {
+		let hc = navigator.hardwareConcurrency;
+		dom.nHWC.innerHTML = (hc == "2" ? hc + rfp_green : hc + rfp_red);
+	} else {
+		dom.nHardware = "disabled";
+	}
+
+	// pointers
+	dom.pointer1 = (window.PointerEvent == "undefined" ? "disabled" : "enabled");
+	dom.mmP = get_mm_pointer("any-pointer") + " | " + get_mm_pointer("pointer");
 
 	// touch
 	dom.touch1 = navigator.maxTouchPoints + " | "
@@ -79,10 +218,9 @@ function outputDevices() {
 		+ ("ontouchend" in window) + " | "
 		+ get_touch_event();
 
-
 	// perf
 	let t1 = performance.now();
-	if (sPerf) {outputDebug("1", "devices", (t1-t0), (t1 - gt0))};
+	outputDebug("1", "devices", (t1-t0), (t1 - gt0));
 };
 
 outputDevices();
