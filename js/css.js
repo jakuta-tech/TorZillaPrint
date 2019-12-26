c503cff416e2966602661cb13c8c4ec151e05521
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-12-26 13:34:03 +0000

    Update css.js

--- js/css.js
+++ js/css.js
@@ -2,26 +2,7 @@
 
 'use strict';
 
-function get_prefers_color_scheme(){
-	let e = dom.pColorElement;
-	let k = window.getComputedStyle(e, null).getPropertyValue("background-color");
-	if (k == "rgb(255, 255, 255)") {k = "light" + rfp_green}
-	else if (k == "rgb(255, 0, 0)") {k = "not supported"}
-	else if (k == "rgb(0, 0, 0)") {k = "dark" + rfp_red}
-	else if (k == "rgb(0, 0, 255)") {k = "no-preference" + rfp_red};
-	dom.pColorScheme.innerHTML = k;
-};
-
-function get_prefers_reduced_motion() {
-	let e = dom.pMotionElement;
-	let k = window.getComputedStyle(e, null).getPropertyValue("background-color");
-	if (k == "rgb(255, 255, 255)") {k = "no-preference" + rfp_green}
-	else if (k == "rgb(255, 0, 0)") {k = "not supported"}
-	else if (k == "rgb(0, 0, 0)") {k = "reduce" + rfp_red};
-	dom.pReducedMotion.innerHTML = k;
-};
-
-function get_system_colors(type) {
+function get_colors(type) {
 	let s = "",
 		array = [],
 		e = dom.sColorElement;
@@ -45,28 +26,42 @@ function get_system_colors(type) {
 	array.forEach(function (item) {
 		e.style.backgroundColor = item;
 		s = s + window.getComputedStyle(e, null).getPropertyValue("background-color")
-		// console.log(window.getComputedStyle(e, null).getPropertyValue("background-color"), item)
 	});
 	s = sha1(s);
 	if (type == "system") {
 		dom.sColorHash.innerHTML = (s == "b5e2344c265fc498d2fb8e0f84951e8d501ad481" ? s + rfp_green : s + rfp_red);
-	} else if (type == "moz") {
-		// ad55ec68f093cb2869190bc1cd18b206424db02b = all rgb(0, 0, 0)
-		dom.mColorHash.innerHTML = s
+	} else {
+		dom.mColorHash = s
 	}
 };
 
+function get_mm_prefers(type){
+	let x = "not supported",
+		l="light", d="dark", n="no-preference", r="reduce",
+		q = "(prefers-"+type+": ";
+	if (type == "color-scheme") {
+		if (window.matchMedia(q+l+")").matches) x = l + rfp_green;
+		if (window.matchMedia(q+d+")").matches) x = d + rfp_red;
+		if (window.matchMedia(q+n+")").matches) x = n + rfp_red;
+		dom.mmPCS.innerHTML = x;
+	}
+	else if (type == "reduced-motion") {
+		if (window.matchMedia(q+r+")").matches) x = r + rfp_red;
+		if (window.matchMedia(q+n+")").matches) x = n + rfp_green;
+		dom.mmPRM.innerHTML = x;
+	}
+};
 
 function outputCSS() {
 	let t0 = performance.now();
 	// functions
-	get_prefers_color_scheme();
-	get_prefers_reduced_motion();
-	get_system_colors("system");
-	get_system_colors("moz");
+	get_mm_prefers("color-scheme");
+	get_mm_prefers("reduced-motion");
+	get_colors("system");
+	get_colors("moz");
 	// perf
 	let t1 = performance.now();
-	if (sPerf) {outputDebug("1", "css", (t1-t0), (t1 - gt0))};
+	outputDebug("1", "css", (t1-t0), (t1 - gt0));
 };
 
 outputCSS();
