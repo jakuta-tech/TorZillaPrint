55e517639038b7ede5c568d84492ab5d5de3f3a3
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-12-26 13:39:32 +0000

    Update media.js

--- js/media.js
+++ js/media.js
@@ -7,11 +7,11 @@ function outputMedia() {
 	let e = "enabled", d = "disabled", ns = "not supported";
 
 	// media.media-capabilities.enabled : FF63+
-	dom.nMediaCapabilities = ("mediaCapabilities" in navigator ? e : (Symbol.for(`foo`).description == "foo" ? d : ns))
+	dom.nMediaC = ("mediaCapabilities" in navigator ? e : (Symbol.for(`foo`).description == "foo" ? d : ns))
 
 	// perf
 	let t1 = performance.now();
-	if (sPerf) {outputDebug("1", "media", (t1-t0), (t1 - gt0))};
+	outputDebug("1", "media", (t1-t0), (t1 - gt0));
 };
 
 outputMedia();
