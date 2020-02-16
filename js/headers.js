a9d982cb198b8d4625967618fa133cc407ec694d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-02-16 08:42:49 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -1,26 +1,21 @@
 /* TABLE: HTTP Headers */
-
 'use strict';
 
 function outputHeaders() {
-	let t0 = performance.now();
-
-	dom.nDNT = navigator.doNotTrack;
-	dom.nOnLine = navigator.onLine;
-
-	// network info api: dom.netinfo.enabled
+	let t0 = performance.now()
+	dom.nDNT = navigator.doNotTrack
+	dom.nOnLine = navigator.onLine
+	// network info api
 	if ("connection" in navigator) {
-		dom.nNetwork = "enabled";
-		let s = navigator.connection.type;
-		dom.nConnection.innerHTML = (s == "unknown" ? s + rfp_green : s + rfp_red)
+		dom.nNetwork = zE
+		let r = navigator.connection.type
+		dom.nConnection.innerHTML = r += (r == "unknown" ? rfp_green : rfp_red)
 	} else {
-		dom.nNetwork = "disabled";
-		dom.nConnection = navigator.connection;
-	};
-
+		dom.nNetwork = zD
+		dom.nConnection = navigator.connection
+	}
 	// perf
-	let t1 = performance.now();
-	outputDebug("1", "headers", (t1-t0), (t1 - gt0));
-};
+	debug_page("perf","headers",t0,gt0)
+}
 
-outputHeaders();
+outputHeaders()
