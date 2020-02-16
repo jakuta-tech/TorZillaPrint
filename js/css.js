1670f84bf6ec7555c53e29174d7c66042c264b8b
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-02-16 08:39:54 +0000

    Update css.js

--- js/css.js
+++ js/css.js
@@ -1,19 +1,18 @@
 /* TABLE: CSS */
-
 'use strict';
 
 function get_colors(type) {
-	let s = "",
-		array = [],
-		e = dom.sColorElement;
+	let results = [],
+		list = [],
+		element = dom.sColorElement
 	if (type == "system") {
-		array = ['ActiveBorder','ActiveCaption','AppWorkspace','Background','ButtonFace',
+		list = ['ActiveBorder','ActiveCaption','AppWorkspace','Background','ButtonFace',
 		'ButtonHighlight','ButtonShadow','ButtonText','CaptionText','GrayText','Highlight',
 		'HighlightText','InactiveBorder','InactiveCaption', 'InactiveCaptionText','InfoBackground',
 		'InfoText','Menu','MenuText','Scrollbar','ThreeDDarkShadow','ThreeDFace','ThreeDHighlight',
 		'ThreeDLightShadow','ThreeDShadow','Window','WindowFrame','WindowText']
 	} else if (type == "moz") {
-		array = ['-moz-ButtonDefault','-moz-ButtonHoverFace','-moz-CellHighlight','-moz-CellHighlightText',
+		list = ['-moz-ButtonDefault','-moz-ButtonHoverFace','-moz-CellHighlight','-moz-CellHighlightText',
 		'-moz-Combobox','-moz-ComboboxText','-moz-Dialog','-moz-DialogText','-moz-DragTargetZone',
 		'-moz-EvenTreeRow','-moz-Field','-moz-FieldText','-moz-MenuHover','-moz-MenuHoverText','-moz-MenubarText',
 		'-moz-NativeHyperlinkText','-moz-OddTreeRow','-moz-html-CellHighlight','-moz-html-CellHighlightText',
@@ -23,29 +22,29 @@ function get_colors(type) {
 		'-moz-win-MediaText','-moz-win-CommunicationsText','-moz-ActiveHyperlinkText','-moz-HyperLinkText',
 		'-moz-VisitedHyperlinkText','-moz-default-background-color','-moz-default-color']
 	}
-	array.forEach(function (item) {
-		e.style.backgroundColor = item
-		s = s+window.getComputedStyle(e, null).getPropertyValue("background-color")
-	});
-	s = sha1(s)
+	list.forEach(function (item) {
+		element.style.backgroundColor = item
+		results.push(window.getComputedStyle(element, null).getPropertyValue("background-color"))
+	})
+	let hash = sha1(results.join())
 	if (type == "system") {
-		dom.sColorHash.innerHTML = (s == "b5e2344c265fc498d2fb8e0f84951e8d501ad481" ? s + rfp_green : s + rfp_red)
+		dom.sColorHash.innerHTML = hash += (hash == "c833ed5e44c6da5e9fc2964259bf34b280891b73" ? rfp_green : rfp_red)
 	} else {
-		dom.mColorHash = s
+		dom.mColorHash = hash
 	}
 }
 
 function get_mm_prefers(type){
-	let x = "not supported", l="light", d="dark", n="no-preference", r="reduce", q = "(prefers-"+type+": ";
+	let x=zNS, l="light", d="dark", n="no-preference", r="reduce", q="(prefers-"+type+": "
 	if (type == "color-scheme") {
-		if (window.matchMedia(q+l+")").matches) x = l + rfp_green
-		if (window.matchMedia(q+d+")").matches) x = d + rfp_red
-		if (window.matchMedia(q+n+")").matches) x = n + rfp_red
+		if (window.matchMedia(q+l+")").matches) x = l+rfp_green
+		if (window.matchMedia(q+d+")").matches) x = d+rfp_red
+		if (window.matchMedia(q+n+")").matches) x = n+rfp_red
 		dom.mmPCS.innerHTML = x
 	}
-	else if (type == "reduced-motion") {
-		if (window.matchMedia(q+r+")").matches) x = r + rfp_red
-		if (window.matchMedia(q+n+")").matches) x = n + rfp_green
+	if (type == "reduced-motion") {
+		if (window.matchMedia(q+r+")").matches) x = r+rfp_red
+		if (window.matchMedia(q+n+")").matches) x = n+rfp_green
 		dom.mmPRM.innerHTML = x
 	}
 }
@@ -58,8 +57,7 @@ function outputCSS() {
 	get_colors("system")
 	get_colors("moz")
 	// perf
-	let t1 = performance.now()
-	outputDebug("1","css",(t1-t0),(t1-gt0))
+	debug_page("perf","css",t0,gt0)
 }
 
 outputCSS()
