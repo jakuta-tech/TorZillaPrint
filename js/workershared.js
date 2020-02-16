1197866f4f070e3d6e660b6d9a476596cdf8113a
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-02-16 08:47:04 +0000

    Update workershared.js

--- js/workershared.js
+++ js/workershared.js
@@ -1,9 +1,10 @@
 /* SHARED WORKER SCRIPT */
 'use strict';
-var ports = [];
+
+var ports = []
 onconnect = function(e) {
-	var port = e.ports[0];
-	ports.push(port);
-	port.start();
-	port.onmessage = function(e) {port.postMessage("TZP-"+e.data)};
-};
+	let port = e.ports[0]
+	ports.push(port)
+	port.start()
+	port.onmessage = function(e) {port.postMessage("TZP-"+e.data)}
+}
