4f334cb0d8097699eca3dbc1e16e2e8d7b6a58b7
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-02-16 08:40:26 +0000

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -1,282 +1,250 @@
 /* TABLE: Devices & Hardware */
-
 'use strict';
 
 function reset_devices() {
-	// hide w/ color: don't shrink elements
-	dom.mimeTypes.style.color = zhide;
-	dom.plugins.style.color = zhide;
-	dom.eMediaDevices.style.color = zhide;
+	// hide/color: dont shrink elements
+	dom.mimeTypes.style.color = zhide
+	dom.plugins.style.color = zhide
+	dom.eMD.style.color = zhide
 	// hide notation
-	let str = dom.eMediaDevices.innerHTML
-	str = str.replace(/\[RFP\]/g, "");
-	dom.eMediaDevices.innerHTML = str;
-};
+	let str = dom.eMD.innerHTML
+	str = str.replace(/\[RFP\]/g, "")
+	dom.eMD.innerHTML = str
+}
 
 function get_gamepads() {
 	if ("getGamepads" in navigator) {
-		dom.nGetGamepads = "enabled";
-		let gamepads = navigator.getGamepads();
-		if (gamepads.length == 0) {
-			dom.eGamepads.innerHTML = "none" + rfp_green
-		} else {
-			// ToDo: gamepads: enumerate
-			let items = (gamepads.length == 1 ? " item]" : " items]");
-			dom.eGamepads.innerHTML = note_ttc + " [" + gamepads.length + items + rfp_red
-			//gamepads.forEach(function(gamepad) {
-			//});
-		}
+		dom.nGP = zE
+		// ToDo: listen for gamepads
+		dom.eGP.innerHTML = note_ttc
 	} else {
-		dom.nGetGamepads = "disabled";
-		dom.eGamepads = "n/a";
+		dom.nGP = zD; dom.eGP = zNA
+	}
+}
+
+function get_hardware_concurrency() {
+	if ("hardwareConcurrency" in navigator) {
+		let h = navigator.hardwareConcurrency
+		dom.nHWC.innerHTML = (h == "2" ? h + rfp_green : h + rfp_red)
+	} else {
+		dom.nHWC = zD
 	}
 }
 
 function get_media_devices() {
-	// media.navigator.enabled
 	if ("mediaDevices" in navigator) {
-		dom.nMediaDevices = "enabled";
+		dom.nMD = zE
 		// enumerate
-		let str = "", pad = 0, strPad = "";
+		let str="", pad=0, strPad=""
 		navigator.mediaDevices.enumerateDevices().then(function(devices) {
-			let aCount = 0, vCount = 0, oCount = 0;
-			devices.forEach(function(device) {
-				if (device.kind == "audioinput") {aCount++}
-				else if (device.kind == "videoinput") {vCount++}
-				else {oCount++}
-				pad = device.kind.length + 2;
-				str += device.kind + ": " + device.deviceId
-				if (device.groupId.length > 0) {
-					strPad = "group: "; strPad = strPad.padStart(pad)
-					str += "<br>" + strPad + device.groupId
+			let aCount=0, vCount=0, oCount=0
+			devices.forEach(function(d) {
+				if (d.kind == "audioinput") {aCount++}
+				else if (d.kind == "videoinput") {vCount++}	else {oCount++}
+				pad = d.kind.length + 2
+				str += d.kind+": "+d.deviceId
+				if (d.groupId.length > 0) {
+					strPad = ("group: ").padStart(pad)
+					str += "<br>"+strPad+d.groupId
 				}
-				if (device.label.length > 0) {
-					strPad = "label: "; strPad = strPad.padStart(pad)
-					str += "<br>" + strPad + device.label
+				if (d.label.length > 0) {
+					strPad = ("label: ").padStart(pad)
+					str += "<br>"+strPad+d.label
 				}
 				str += "<br>"
-			});
+			})
 			// rfp
 			if (isFF) {
 				if (aCount == 1 && vCount == 1 && oCount == 0) {
-					str = str.replace("<br>", rfp_green + "<br>");
+					str = str.replace("<br>", rfp_green+"<br>")
 				} else {
-					str = str.replace("<br>", rfp_red + "<br>");
+					str = str.replace("<br>", rfp_red+"<br>")
 				}
 			}
-			dom.eMediaDevices.innerHTML = str
+			dom.eMD.innerHTML = str
 		})
-		.catch(function(err) {
-			console.debug("enumerateDevices", err.name, err.message);
-		});
+		.catch(function(e) {
+			console.debug("enumerateDevices", e.name, e.message)
+		})
 	}	else {
-		dom.nMediaDevices = "disabled";
-		dom.eMediaDevices = "n/a";
+		dom.nMD = zD; dom.eMD = zNA
 	}
-	dom.eMediaDevices.style.color = zshow;
-};
+	dom.eMD.style.color = zshow
+}
 
 function get_mimetypes() {
 	if ("mimeTypes" in navigator) {
-		let mimes = navigator.mimeTypes
-		if (mimes.length == 0) {
-			dom.mimeTypes.innerHTML = "none"
-		} else {
-			let str = "";
-			for (let i = 0; i < mimes.length; i++) {
-				str += mimes[i].type
-				if (mimes[i].description != "") {str += ": " + mimes[i].type}
-				if (mimes[i].suffixes != "") {str += ": " + mimes[i].suffixes}
-				str += "<br>";
+		let m = navigator.mimeTypes
+		if (m.length > 0) {
+			let s=""
+			for (let i=0; i < m.length; i++) {
+				s += m[i].type + (m[i].description == "" ? ": * " : ": " + m[i].type)
+					+ (m[i].suffixes == "" ? ": *" : ": " + m[i].suffixes) + "<br>"
 			}
-			dom.mimeTypes.innerHTML = str
+			dom.mimeTypes.innerHTML = s
+		} else {
+			dom.mimeTypes.innerHTML = "none"
 		}
 	} else {
-		dom.mimeTypes = "disabled";
+		dom.mimeTypes = zD
 	}
-	dom.mimeTypes.style.color = zshow;
-};
+	dom.mimeTypes.style.color = zshow
+}
 
 function get_mm_hover(type){
-	let x = "not supported", h="hover", n="none", q = "("+type+": ";
-	if (window.matchMedia(q+n+")").matches) x = n;
-	if (window.matchMedia(q+h+")").matches) x = h;
+	let x=zNS, h="hover", n="none", q="("+type+":"
+	if (window.matchMedia(q+n+")").matches) x=n
+	if (window.matchMedia(q+h+")").matches) x=h
 	return x
-};
+}
 
 function get_mm_pointer(type){
-	let x = "not supported", f="fine", c="coarse", n="none", q = "("+type+": ";
-	if (window.matchMedia(q+n+")").matches) x = n;
-	if (window.matchMedia(q+c+")").matches) x = c;
-	if (window.matchMedia(q+f+")").matches) x = f;
+	let x=zNS, f="fine", c="coarse", n="none", q="("+type+": "
+	if (window.matchMedia(q+n+")").matches) x=n
+	if (window.matchMedia(q+c+")").matches) x=c
+	if (window.matchMedia(q+f+")").matches) x=f
 	return x
-};
-
-function get_mm_touch(){
-	let x = "not supported", q = "(-moz-touch-enabled: ";
-	if (window.matchMedia(q+"0)").matches) x = 0;
-	if (window.matchMedia(q+"1)").matches) x = 1;
-	return x
-};
+}
 
 function get_plugins() {
 	if ("plugins" in navigator) {
-		if (navigator.plugins.length == 0) {
-			dom.plugins.innerHTML = "none";
-		} else {
-			let str = "";
-			for (let i = 0; i < navigator.plugins.length; i++) {
-				str += navigator.plugins[i].name
-				if (navigator.plugins[i].filename == "") {
-					str += ": - : "
-				} else {
-					str += ": " + navigator.plugins[i].filename
-				}
-				if (navigator.plugins[i].description == "") {
-					str += ": - : "
-				} else {
-					str += ": " + navigator.plugins[i].description
-				}
-				str += "<br>"
+		let p = navigator.plugins
+		if (p.length > 0) {
+			let s=""
+			for (let i=0; i < p.length; i++) {
+				s += p[i].name + (p[i].filename == "" ? ": * " : ": " + p[i].filename)
+					+ (p[i].description == "" ? ": *" : ": " + p[i].description) + "<br>"
 			}
-			dom.plugins.innerHTML = str
+			dom.plugins.innerHTML = s
+		} else {
+			dom.plugins.innerHTML = "none"
 		}
 	} else {
-		dom.plugins = "disabled";
+		dom.plugins = zD
 	}
-	dom.plugins.style.color = zshow;
+	dom.plugins.style.color = zshow
+}
+
+function get_pointer_hover() {
+	// FF64: pointer/hover
+	dom.pointer = (window.PointerEvent == "undefined" ? zD : zE)
+	let p = get_mm_pointer("any-pointer")+" | "+get_mm_pointer("pointer")
+	let h = get_mm_hover("any-hover")+" | "+get_mm_hover("hover")
+	// 1607316
+	if (isVer > 73 && isOS == "android") {
+		p += (p == "coarse | coarse" ? rfp_green : rfp_red)
+		h += (h == "none | none" ? rfp_green : rfp_red)
+	} else {
+		let rfp = zNS+" | "+zNS
+		if (p !== rfp) {
+			p += (p == "fine | fine" ? rfp_green : rfp_red)
+		}
+		if (h !== rfp) {
+			h += (h == "hover | hover" ? rfp_green : rfp_red)
+		}
+	}
+	dom.mmP.innerHTML = p
+	dom.mmH.innerHTML = h
 }
 
 function get_speech_synth() {
-	// media.webspeech.synth.enabled
 	if ("speechSynthesis" in window) {
-		dom.speechSynth = "enabled";
+		dom.sSynth = zE
 		// speech engines
 		function populateVoiceList() {
 			if(typeof speechSynthesis === 'undefined') {
-				return;
+				return
 			}
-			let voices = speechSynthesis.getVoices();
-			let str = "";
-			for (let i = 0; i < voices.length ; i++) {
-				str += voices[i].name + " (" + voices[i].lang + ")";
-				if (voices[i].default) { str += " : default" }
-				str += "<br>"
+			let v = speechSynthesis.getVoices()
+			let s=""
+			for (let i=0; i < v.length; i++) {
+				s += v[i].name + " (" + v[i].lang + ")" + (v[i].default ? " : default" : "") + "<br>"
 			}
 			if (isFF) {
-				if (str == "") {
-					str = "none" + rfp_green; // RFP: 1333641
+				if (s == "") {
+					s = "none" + rfp_green // RFP: 1333641
 				} else {
-					str = str.replace("<br>", rfp_red + "<br>");
+					s = s.replace("<br>", rfp_red + "<br>")
 				}
 			}
-			dom.speechEngines.innerHTML = str
+			dom.sEngines.innerHTML = s
 		}
-		populateVoiceList();
-		if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
-			speechSynthesis.onvoiceschanged = populateVoiceList;
+		populateVoiceList()
+		if (typeof speechSynthesis !== "undefined" && speechSynthesis.onvoiceschanged !== undefined) {
+			speechSynthesis.onvoiceschanged = populateVoiceList
 		}
 	} else {
-		dom.speechSynth = "disabled";
-		dom.speechEngines = "n/a";
+		dom.sSynth = zD; dom.sEngines = zNA
 	}
-};
+}
 
 function get_speech_rec() {
 	// media.webspeech.recognition.enable
 	// NOTE: media.webspeech.test.enable until landed
 	try {
-		let recognition = new SpeechRecognition();
-		dom.speechRec = "enabled";
+		let recognition = new SpeechRecognition()
+		dom.sRec = zE
 	} catch(e) {
 		// undefined
-		// ToDo: speech recognition: can I detect disabled vs not supported
-		dom.speechRec = "disabled [or not supported]"
+		// ToDo: speechRec: detect dis-abled vs not-supported?
+		dom.sRec = zD+" [or "+zNS+"]"
 	}
-};
+}
 
-function get_touch_event() {
-	// ToDo: TouchEvent: do i remove it on fail?
+function get_touch() {
+	// vars
+	let m = zNS,
+		q="(-moz-touch-enabled:",
+		t = false
+	// m
+	if (window.matchMedia(q+"0)").matches) m=0
+	if (window.matchMedia(q+"1)").matches) m=1
+	// t
 	try {
-		document.createEvent("TouchEvent");
-		return true
-	} catch (e) {
-		return false
-	}
-};
+		document.createEvent("TouchEvent")
+		t = true
+	} catch (e) {}
+	// output
+	dom.touch = navigator.maxTouchPoints +" | "+ m +" | "
+		+("ontouchstart" in window)+" | "+("ontouchend" in window)+" | "+ t
+}
 
 function get_vr() {
-	// dom.vr.enabled
 	if ("getVRDisplays" in navigator) {
-		dom.nGetVR = "enabled";
+		dom.nVR = zE
 		if ("activeVRDisplays" in navigator) {
-			let displays = navigator.activeVRDisplays;
-			if (displays.length == 0) {
-				dom.nActiveVR = "none"
+			let d = navigator.activeVRDisplays
+			if (d.length == 0) {
+				dom.aVR = "none"
 			} else {
-				// ToDo: activeVRDisplays: enumerate
-				let items = (displays.length == 1 ? " item]" : " items]");
-				dom.nActiveVR.innerHTML = note_ttc + " [" + displays.length + items;
-				for (let i = 0; i < displays.length; i++) {
-					// console.debug(displays[i].displayId);
+				// ToDo: VR: enum
+				let items = " item" + (d.length == 1 ? "" : "s") + "]"
+				dom.aVR.innerHTML = note_ttc+" ["+d.length + items
+				for (let i=0; i < d.length; i++) {
+					// console.debug(d[i].displayId)
 				}
 			}
 		}
 	}	else {
-		dom.nGetVR = "disabled";
-		dom.nActiveVR = "n/a";
+		dom.nVR = zD; dom.aVR = zNA
 	}
-};
+}
 
 function outputDevices() {
-	let t0 = performance.now();
-
-	get_gamepads();
-	get_media_devices();
-	get_mimetypes();
-	get_plugins();
-	get_speech_synth();
-	get_speech_rec();
-	get_vr();
-
-	// hardwareConcurrency
-	if ("hardwareConcurrency" in navigator) {
-		let hc = navigator.hardwareConcurrency;
-		dom.nHWC.innerHTML = (hc == "2" ? hc + rfp_green : hc + rfp_red);
-	} else {
-		dom.nHardware = "disabled";
-	}
-
-	// FF64: pointer/hover
-	dom.pointer1 = (window.PointerEvent == "undefined" ? "disabled" : "enabled");
-	let strP = get_mm_pointer("any-pointer") + " | " + get_mm_pointer("pointer");
-	let strH = get_mm_hover("any-hover") + " | " + get_mm_hover("hover");
-	// 1607316
-	// ToDo: pointer/hover: watch for tor #32886 to land
-	if (isVer > 73 && isOS == "android") {
-		strP = (strP == "coarse | coarse" ? strP + rfp_green : strP + rfp_red);
-		strH = (strH == "none | none" ? strH + rfp_green : strH + rfp_red);
-	} else {
-		if (strP !== "not supported | not supported") {
-			strP = (strP == "fine | fine" ? strP + rfp_green : strP + rfp_red);
-		}
-		if (strH !== "not supported | not supported") {
-			strH = (strH == "hover | hover" ? strH + rfp_green : strH + rfp_red);
-		}
-	}
-	dom.mmP.innerHTML = strP;
-	dom.mmH.innerHTML = strH;
-
-	// touch
-	dom.touch1 = navigator.maxTouchPoints + " | "
-		+ get_mm_touch() + " | "
-		+ ("ontouchstart" in window) + " | "
-		+ ("ontouchend" in window) + " | "
-		+ get_touch_event();
-
+	let t0 = performance.now()
+	// run
+	get_gamepads()
+	get_hardware_concurrency()
+	get_media_devices()
+	get_mimetypes()
+	get_plugins()
+	get_pointer_hover()
+	get_speech_rec()
+	get_speech_synth()
+	get_touch()
+	get_vr()
 	// perf
-	let t1 = performance.now();
-	outputDebug("1", "devices", (t1-t0), (t1 - gt0));
-};
+	debug_page("perf","devices",t0,gt0)
+}
 
-outputDevices();
+outputDevices()
