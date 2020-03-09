4a39bd51f46d42d0bfdef8666602a9eabceac587
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-03-09 02:24:41 +0000

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -16,11 +16,12 @@ function getUniqueElements() {
 function store_data(section, key, value) {
 	//if (value == "") {value = "unknown"}
 	if (section == "ua") {
-		//console.log(section.padStart(10),key.padStart(20),value)
+		//console.log(section.padStart(4),key.padStart(8),value)
 		fp_ua.push(key+":"+value)
-		if (fp_ua.length == 7) {
-			fp_ua.sort() // by key
-			dom.sectionUA2.innerHTML = sha1(fp_ua.join()) + so+"[can't be spoofed]"+sc
+		if (fp_ua.length == 5) {
+			fp_ua.sort()
+			//console.debug(fp_ua.join())
+			dom.sectionUA3.innerHTML = sha1(fp_ua.join()) + s2+"[unspoofable?]"+sc
 		}
 	}
 }
@@ -60,7 +61,7 @@ function debug_page(target, str1, str2, str3, str4) {
 			let time1 = (t0-str2).toString()
 			str = str1.padStart(12) + ": " + sn + time1.padStart(4) + sc + " ms"
 			if (str3 !== undefined && str3 !== "") {
-				// only output running time if not a section rerun
+				// output running time if not a section rerun
 				if (gRerun == false) {
 					let time2 = (t0-str3).toString()
 					str = str + " | " + so + time2.padStart(4) + sc + " ms"
@@ -72,8 +73,10 @@ function debug_page(target, str1, str2, str3, str4) {
 			}
 			e.innerHTML = e.innerHTML + "<br>" + str
 		} else {
-			str = str1
-			e.innerHTML = e.innerHTML + str + "<br>"
+			if (gRerun == false) {
+				str = str1
+				e.innerHTML = e.innerHTML + str + "<br>"
+			}
 		}
 	}
 }
@@ -92,66 +95,74 @@ function showhide(togType, togID, togWord) {
 			document.getElementById("label"+togID).innerHTML = togWord+" details"
 		}
 	}
+	// errors
+	if (togID == "E") {
+		if (dom.err5.textContent.length > 1) {
+			dom.togE5.style.display = "table=row"
+		} else {
+			dom.togE5.style.display = "none"
+		}
+	}
 	// domrect show/hide extra sections & change drFirstHeader text
 	if (togID == "D") {
-		let drArray = [dom.dr1.innerHTML, dom.dr2.innerHTML, dom.dr3.innerHTML, dom.dr4.innerHTML];
-		var xyz = document.getElementsByClassName("togD1"); var abc;
+		let drArray = [dom.dr1.innerHTML, dom.dr2.innerHTML, dom.dr3.innerHTML, dom.dr4.innerHTML]
+		let xyz = document.getElementsByClassName("togD1"); let abc
 		if (drArray.every( (val, i, arr) => val === arr[0] )) {
 			// hide last three
-			dom.drFirstHeader.innerHTML = "Element.getClientRects"+`<br>`+"[note: the other three methods are identical]";
-			for (abc = 0; abc < xyz.length; abc++) { xyz[abc].style.display = "none";};
+			dom.drFirstHeader.innerHTML = "Element.getClientRects"+`<br>`+"[note: the other three methods are identical]"
+			for (abc = 0; abc < xyz.length; abc++) { xyz[abc].style.display = "none"}
 		} else {
 			// display last three
-			dom.drFirstHeader.innerHTML = "Element.getClientRects";
-			for (abc = 0; abc < xyz.length; abc++) { xyz[abc].style.display = togType;};
-		};
-	};
+			dom.drFirstHeader.innerHTML = "Element.getClientRects"
+			for (abc = 0; abc < xyz.length; abc++) { xyz[abc].style.display = togType;}
+		}
+	}
 	// font lists show/hide if same hash or not, and change label text
 	if (togID == "F") {
 		if (isPage == "main") {
-			let fontA = dom.small_fontFPJS2.innerHTML;
-			let fontB = dom.small_fontFB.innerHTML;
+			let fontA = dom.small_fontFPJS2.innerHTML
+			let fontB = dom.small_fontFB.innerHTML
 			if (fontA == fontB) {
 				// same: hide the second
-				dom.small_fontlabel = "whitelist";
-				dom.fontB1.style.display = "none";
-				dom.fontB2.style.display = "none";
+				dom.small_fontlabel = "whitelist"
+				dom.fontB1.style.display = "none"
+				dom.fontB2.style.display = "none"
 			} else {
 				// different: show both
-				dom.small_fontlabel = "fingerprintjs2 [whitelist]";
-				dom.fontB1.style.display = togType;
-				dom.fontB2.style.display = togType;
-			};
-			let fontC = dom.all_fontFPJS2.innerHTML;
-			let fontD = dom.all_fontFB.innerHTML;
+				dom.small_fontlabel = "fingerprintjs2 [whitelist]"
+				dom.fontB1.style.display = togType
+				dom.fontB2.style.display = togType
+			}
+			let fontC = dom.all_fontFPJS2.innerHTML
+			let fontD = dom.all_fontFB.innerHTML
 			if (fontC == fontD) {
 				// same: hide the second
 				dom.all_fontlabel = "os";
-				dom.fontD1.style.display = "none";
-				dom.fontD2.style.display = "none";
+				dom.fontD1.style.display = "none"
+				dom.fontD2.style.display = "none"
 			} else {
 				// different: show both
-				dom.all_fontlabel = "fingerprintjs2 [os]";
-				dom.fontD1.style.display = togType;
-				dom.fontD2.style.display = togType;
-			};
+				dom.all_fontlabel = "fingerprintjs2 [os]"
+				dom.fontD1.style.display = togType
+				dom.fontD2.style.display = togType
+			}
 		} else if (isPage == "extra") {
-			let fontE = dom.monsta_fontFPJS2.innerHTML;
-			let fontF = dom.monsta_fontFB.innerHTML;
+			let fontE = dom.monsta_fontFPJS2.innerHTML
+			let fontF = dom.monsta_fontFB.innerHTML
 			if (fontE == fontF) {
 				// same: hide the second
-				dom.monsta_fontlabel = "monsta";
-				dom.fontF1.style.display = "none";
-				dom.fontF2.style.display = "none";
+				dom.monsta_fontlabel = "monsta"
+				dom.fontF1.style.display = "none"
+				dom.fontF2.style.display = "none"
 			} else {
 				// different: show both
-				dom.monsta_fontlabel = "fingerprintjs2 [monsta]";
-				dom.fontF1.style.display = togType;
-				dom.fontF2.style.display = togType;
-			};
+				dom.monsta_fontlabel = "fingerprintjs2 [monsta]"
+				dom.fontF1.style.display = togType
+				dom.fontF2.style.display = togType
+			}
 		}
-	};
-};
+	}
+}
 
 function toggleitems(chkbxState, chkbxID) {
 	if (chkbxState.checked) {
@@ -159,20 +170,20 @@ function toggleitems(chkbxState, chkbxID) {
 		if (chkbxID=="F") {stateFNT = false}
 		showhide("none",chkbxID,"&#9660; show")
 	} else {
-		if (chkbxID == "D") {stateDR = true}
-		if (chkbxID == "F") {stateFNT = true}
+		if (chkbxID=="D") {stateDR = true}
+		if (chkbxID=="F") {stateFNT = true}
 		showhide("table-row",chkbxID,"&#9650; hide")
 	}
 }
 
 function copyclip(element) {
 	if (document.selection) {
-		var range = document.body.createTextRange()
+		let range = document.body.createTextRange()
 		range.moveToElementText(document.getElementById(element))
 		range.select().createTextRange()
 		document.execCommand("copy")
 	} else if (window.getSelection) {
-		var range = document.createRange()
+		let range = document.createRange()
 		range.selectNode(document.getElementById(element))
 		window.getSelection().addRange(range)
 		document.execCommand("copy")
@@ -208,8 +219,8 @@ function sha1(str1){
 		for(i=5;i;) H[--i] = H[i] + A[i] | 0;
 	}
 	for(str1='';i<40;)str1 += (H[i>>3] >> (7-i++%8)*4 & 15).toString(16);
-	return str1;
-};
+	return str1
+}
 
 /* BASE64 STUFF */
 /* Base64 / binary data / UTF-8 strings utilities (#3)
@@ -233,7 +244,7 @@ function byteArrayToHex(arrayBuffer){
 	return chunks.map(function(chunk){
 		return "0".repeat(8 - chunk.length) + chunk;
 	}).join("");
-};
+}
 
 /* BUTTONS: (re)GENERATE SECTIONS */
 function outputSection(id, cls, page) {
@@ -241,7 +252,7 @@ function outputSection(id, cls, page) {
 	// clear elements, &nbsp stops line height jitter
 	let tbl = document.getElementById("tb"+id)
 	tbl.querySelectorAll(`.${cls}`).forEach(e => {e.innerHTML = "&nbsp"})
-	// clear details if applicable
+	// clear details
 	if (page == "m") {
 		if (id=="1") {dom.kbt.value = ""}
 		if (id=="7") {reset_devices()}
@@ -251,11 +262,10 @@ function outputSection(id, cls, page) {
 		if (id=="18") {reset_misc()}
 	}
 
-	// delay output so users can see something happened
-	let delay = 170
+	// wait so users see change
 	function call_output() {
 		clearInterval(checking)
-		// reset global timer
+		// reset timer
 		gt0 = performance.now()
 		if (page=="m") {
 			if (id=="1") {outputScreen("screen")}
@@ -263,7 +273,7 @@ function outputSection(id, cls, page) {
 			if (id=="3") {outputMath()}
 			if (id=="4" && cls=="c") {outputLanguage()}
 			if (id=="5") {outputHeaders()}
-			if (id=="6") {outputCookies()}
+			if (id=="6") {outputStorage()}
 			if (id=="7") {outputDevices()}
 			if (id=="8") {outputCanvas()}
 			if (id=="9") {outputDomRect()}
@@ -279,13 +289,15 @@ function outputSection(id, cls, page) {
 			if (id=="6") {outputWidgets()}
 		}
 	}
-	let checking = setInterval(call_output, delay)
+	let checking = setInterval(call_output, 170)
 
-	// don't delay these ones
+	// don't wait
 	if (page=="m") {
 		if (id=="11" && cls=="c2") {outputFonts2("small")}
 		if (id=="11" && cls=="c3") {outputFonts2("all")}
 	} else if (page=="e") {
+		isPage = "extra"
+		if (id=="1") {outputScreen()}
 		if (id=="2") {outputResource()}
 		if (id=="3") {outputChrome()}
 		if (id=="5") {outputFonts2("monsta")}
