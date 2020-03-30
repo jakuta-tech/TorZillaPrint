720639dceda3448b70ec7a651aa02b52b316488c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-03-30 14:51:09 +0000

    Update iframe_ua.js

--- js/iframe_ua.js
+++ js/iframe_ua.js
@@ -1,11 +1,3 @@
 'use strict';
 
-let list = ['userAgent','appCodeName','appName','product','appVersion',
-	'oscpu','platform','buildID','productSub','vendor','vendorSub'],
-	res = []
-for(let i=0; i < list.length; i++) {
-	let r = navigator[list[i]]
-	if (r == "") {r = "undefined"}
-	res.push((i).toString().padStart(2,"0")+" "+r)
-}
-document.getElementById("result").innerHTML = sha1(res.join())
+document.getElementById("result").innerHTML = get_ua_hash("")
