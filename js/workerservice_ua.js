6c28a8e469b74f1308b2dc6baf373179f2b7fd40
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-03-31 05:42:36 +0000

    Create workerservice_ua.js

---
+++ js/workerservice_ua.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.debug("sw ua reporting")
+addEventListener("message", function(e) {
+	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
+		res = []
+	for (let i=0; i < list.length; i++) {
+		let r = navigator[list[i]]
+		if (r == "") {r = "undefined"}
+		res.push((i).toString().padStart(2,"0")+" "+r)
+	}
+	self.postMessage(res)
+}, false)
