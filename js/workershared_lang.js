3ae2360dbd75cc5d05d6ac45108870e1b797a833
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-11 01:05:46 +0000

    Create workershared_lang.js

---
+++ js/workershared_lang.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var ports = []
+onconnect = function(e) {
+	let res = [],
+		d1 = new Date("January 30, 2019 13:00:00"),
+		d2 = new Date("July 30, 2018 13:00:00")
+	let tz1 = d1.getTimezoneOffset()+ ' | ' + d2.getTimezoneOffset()
+	let tz2 = Intl.DateTimeFormat().resolvedOptions().timeZone
+	res.push(tz1)
+	res.push(tz2)
+	res.push(navigator.languages)
+	res.push(navigator.language)
+	res.push(navigator.languages[0])
+	res.push(new Intl.PluralRules().resolvedOptions().locale)
+	res.push(Intl.DateTimeFormat().resolvedOptions().locale)
+	self.postMessage(res)
+}
+
