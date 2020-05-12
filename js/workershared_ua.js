a85075cf62c268e905033eafa2fea1848ce83d59
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-12 14:18:18 +0000

    Update workershared_ua.js

--- js/workershared_ua.js
+++ js/workershared_ua.js
@@ -8,13 +8,12 @@ onconnect = function(e) {
 	port.onmessage = function(e) {
 		let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 			res = [],
-			zBT = "<span class='bad'>[blocked]</span>",
-			amFF = false
-		// ToDo: only use blocked for amFF: for now extensions can't handle workers
+			zBT = "<span class='bad'>[blocked]</span>"
+		// e.data = isFF
 		for (let i=0; i < list.length; i++) {
 			let r = navigator[list[i]]
 			if (r == "") {r = "undefined"}
-			if (r == undefined && amFF) {r = zBT}
+			if (r == undefined && e.data) {r = zBT}
 			res.push((i).toString().padStart(2,"0")+" "+r)
 		}
 		port.postMessage(res)
