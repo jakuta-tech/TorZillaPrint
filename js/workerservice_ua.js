00e364fe3b44b412dd83053765833598e469474e
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-12 01:00:00 +0000

    Update workerservice_ua.js

--- js/workerservice_ua.js
+++ js/workerservice_ua.js
@@ -8,9 +8,13 @@ addEventListener("message", function(e) {
 	console.debug(performance.now(), "sw: service worker: received message from client, getting nav properties")
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = []
+		zBT = "<span class='bad'>[blocked]</span>",
+		amFF = false
+	// ToDo: only use blocked for amFF: for now extensions can't handle workers
 	for (let i=0; i < list.length; i++) {
 		let r = navigator[list[i]]
 		if (r == "") {r = "undefined"}
+		if (r == undefined && amFF) {r = zBT}
 		res.push((i).toString().padStart(2,"0")+" "+r)
 	}
 	console.debug(performance.now(), "sw: service worker: nav properties done, broadcasting")
