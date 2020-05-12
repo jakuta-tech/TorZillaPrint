b6905d077c885b22ac7b906e2c242b9a8dd448a9
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-12 14:30:19 +0000

    Update workerservice_ua.js

--- js/workerservice_ua.js
+++ js/workerservice_ua.js
@@ -1,11 +1,6 @@
 'use strict';
 
-//self.addEventListener("activate", function(e) {
-//	console.debug(performance.now(), "sw: service worker: activated")
-//})
-
 addEventListener("message", function(e) {
-	//console.debug(performance.now(), "sw: service worker: received message from client, getting nav properties")
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = [],
 		zBT = "<span class='bad'>[blocked]</span>"
@@ -16,8 +11,6 @@ addEventListener("message", function(e) {
 		if (r == undefined && e.data) {r = zBT}
 		res.push((i).toString().padStart(2,"0")+" "+r)
 	}
-	//console.debug(performance.now(), "sw: service worker: nav properties done, broadcasting")
 	let channel = new BroadcastChannel("sw-ua");
 	channel.postMessage({msg: res});
-	//console.debug(performance.now(), "sw: service worker: broadcast sent")
 }, false)
