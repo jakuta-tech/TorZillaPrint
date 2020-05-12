89c3c487f01c618273cf28c934be6aa285207c32
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-12 14:14:09 +0000

    Update worker_ua.js

--- js/worker_ua.js
+++ js/worker_ua.js
@@ -4,8 +4,7 @@ addEventListener("message", function(e) {
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = [],
 		zBT = "<span class='bad'>[blocked]</span>"
-	console.debug("isFF", e.data)
-	if (e.data) {console.debug("banana")}
+	// e.data = isFF
 	for (let i=0; i < list.length; i++) {
 		let r = navigator[list[i]]
 		if (r == "") {r = "undefined"}
