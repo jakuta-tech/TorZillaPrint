87b86a9f5eb709c61a6d42c68bc2579ac087556e
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-12 14:17:57 +0000

    Update workerservice_ua.js

--- js/workerservice_ua.js
+++ js/workerservice_ua.js
@@ -1,24 +1,23 @@
 'use strict';
 
-self.addEventListener("activate", function(e) {
-	console.debug(performance.now(), "sw: service worker: activated")
-})
+//self.addEventListener("activate", function(e) {
+//	console.debug(performance.now(), "sw: service worker: activated")
+//})
 
 addEventListener("message", function(e) {
-	console.debug(performance.now(), "sw: service worker: received message from client, getting nav properties")
+	//console.debug(performance.now(), "sw: service worker: received message from client, getting nav properties")
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = [],
-		zBT = "<span class='bad'>[blocked]</span>",
-		amFF = false
-	// ToDo: only use blocked for amFF: for now extensions can't handle workers
+		zBT = "<span class='bad'>[blocked]</span>"
+	// e.data = isFF
 	for (let i=0; i < list.length; i++) {
 		let r = navigator[list[i]]
 		if (r == "") {r = "undefined"}
-		if (r == undefined && amFF) {r = zBT}
+		if (r == undefined && e.data) {r = zBT}
 		res.push((i).toString().padStart(2,"0")+" "+r)
 	}
-	console.debug(performance.now(), "sw: service worker: nav properties done, broadcasting")
+	//console.debug(performance.now(), "sw: service worker: nav properties done, broadcasting")
 	let channel = new BroadcastChannel("sw-ua");
-	channel.postMessage({msg: res});
-	console.debug(performance.now(), "sw: service worker: broadcast sent")
+	//channel.postMessage({msg: res});
+	//console.debug(performance.now(), "sw: service worker: broadcast sent")
 }, false)
