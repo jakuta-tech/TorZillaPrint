c53cf3d5ddc71b44e5ada9d50e87d0af797a45da
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-12 14:02:30 +0000

    Update worker_ua.js

--- js/worker_ua.js
+++ js/worker_ua.js
@@ -4,8 +4,8 @@ addEventListener("message", function(e) {
 	let list = ['userAgent','appCodeName','appName','product','appVersion','platform'],
 		res = [],
 		zBT = "<span class='bad'>[blocked]</span>",
-		amFF = false
-	// ToDo: only use blocked for amFF: for now extensions can't handle workers
+		amFF = (e == "y" ? true : false)
+	console.debug("I am firefox?", amFF)
 	for (let i=0; i < list.length; i++) {
 		let r = navigator[list[i]]
 		if (r == "") {r = "undefined"}
