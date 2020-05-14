00c7609e681e2d9a8f1005676b5bc014a29b4b5d
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-14 10:04:32 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -1,17 +1,23 @@
 'use strict';
 
 function outputHeaders() {
-	let t0 = performance.now()
-	dom.nDNT = navigator.doNotTrack
-	dom.nOnLine = navigator.onLine
+	let t0 = performance.now(),
+		r = ""
+	// DNT
+	try {r = navigator.doNotTrack} catch(e) {r = "error"}
+	dom.nDNT = r
+	// online
+	try {r = navigator.onLine} catch(e) {r = "error"}
+	dom.nOnLine = r
 	// network info api
 	if ("connection" in navigator) {
 		dom.nNetwork = zE
-		let r = navigator.connection.type
+		try {r = navigator.connection.type} catch(e) {r = "error"}
 		dom.nConnection.innerHTML = r += (r == "unknown" ? rfp_green : rfp_red)
 	} else {
 		dom.nNetwork = zD
-		dom.nConnection = navigator.connection
+		try {r = navigator.connection} catch(e) {r = "error"}
+		dom.nConnection = r
 	}
 	// perf
 	debug_page("perf","headers",t0,gt0)
