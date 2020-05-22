3f9a68269f9766968a7f660b3114eb75e8c98db9
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-22 23:47:51 +0000

    Update storage_workers.js

--- js/storage_workers.js
+++ js/storage_workers.js
@@ -2,12 +2,3 @@
 
 // web worker
 addEventListener("message", function(e) {self.postMessage("TZP-"+e.data)}, false)
-
-// shared
-var ports = []
-onconnect = function(e) {
-	let port = e.ports[0]
-	ports.push(port)
-	port.start()
-	port.onmessage = function(e) {port.postMessage("TZP-"+e.data)}
-}
