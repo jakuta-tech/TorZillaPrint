fb831ec2e98da7a1a59ed75e884c139cb29899f2
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-22 23:40:36 +0000

    Delete workershared.js

--- js/workershared.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ports = []
-onconnect = function(e) {
-	let port = e.ports[0]
-	ports.push(port)
-	port.start()
-	port.onmessage = function(e) {port.postMessage("TZP-"+e.data)}
-}
