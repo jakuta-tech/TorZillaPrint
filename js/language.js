185983561644f3648044db865e5b4015165011bf
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-24 17:11:40 +0000

    Update language.js

--- js/language.js
+++ js/language.js
@@ -2,6 +2,29 @@
 
 let	bTZ = false
 
+function outputHeaders() {
+	let t0 = performance.now(),
+		r = ""
+	// DNT
+	try {r = navigator.doNotTrack} catch(e) {r = "error"}
+	dom.nDNT = r
+	// online
+	try {r = navigator.onLine} catch(e) {r = "error"}
+	dom.nOnLine = r
+	// network info api
+	if ("connection" in navigator) {
+		dom.nNetwork = zE
+		try {r = navigator.connection.type} catch(e) {r = "error"}
+		dom.nConnection.innerHTML = r += (r == "unknown" ? rfp_green : rfp_red)
+	} else {
+		dom.nNetwork = zD
+		try {r = navigator.connection} catch(e) {r = "error"}
+		dom.nConnection = r
+	}
+	// perf
+	debug_page("perf","headers",t0,gt0)
+}
+
 function get_geo() {
 	let r = ("geolocation" in navigator ? zE : zD)
 	dom.geo1 = r
@@ -379,5 +402,5 @@ function outputLanguage() {
 	debug_page("perf","language",t0,gt0)
 }
 
+outputHeaders()
 outputLanguage()
-
