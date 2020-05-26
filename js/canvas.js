7c4ed7ec5d4eec173b41a0a74ed0fb9323fc6648
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-26 21:50:46 +0000

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -60,13 +60,8 @@ function analyzeCanvas(runtype, res1, res2) {
 				value1 += (value1 == "supported" ? rfp_green : rfp_red)
 			}
 			if (sname == "getC") {
-				if (value1.substring(0,5) == "2d: s") {
-					value1 = value1.replace("2d: supported", "2d: supported" + rfp_green)
-				} else {
-					value1 = value1.replace("2d: not supported", "2d: not supported" + rfp_red)
-				} else {
-					value1 += rfp_red
-				}
+				control = "2d: supported"
+				value1 += (value1 == control ? rfp_green : rfp_red )
 			}
 			if (sname == "mozG") {
 				if (isVer > 73) {
