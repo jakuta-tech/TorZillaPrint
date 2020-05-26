c9a7484fc75764e232c79817cc0ff33bafb5a4b9
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-05-26 21:04:20 +0000

    Create language_worker.js

---
+++ js/language_worker.js
@@ -0,0 +1,15 @@
+'use strict';
+
+addEventListener("message", function(e) {
+	let res = [],
+		d1 = new Date("January 30, 2019 13:00:00"),
+		d2 = new Date("July 30, 2018 13:00:00")
+	res.push(d1.getTimezoneOffset()+ ' | ' + d2.getTimezoneOffset())
+	res.push(Intl.DateTimeFormat().resolvedOptions().timeZone)
+	res.push(navigator.languages)
+	res.push(navigator.language)
+	res.push(navigator.languages[0])
+	res.push(new Intl.PluralRules().resolvedOptions().locale)
+	res.push(Intl.DateTimeFormat().resolvedOptions().locale)
+	self.postMessage(res)
+}, false)
