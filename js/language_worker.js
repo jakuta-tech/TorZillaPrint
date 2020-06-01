d550edcb40cd8fec9ad3193651a69df51e0b157b
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2020-06-01 14:05:06 +0000

    Update language_worker.js

--- js/language_worker.js
+++ js/language_worker.js
@@ -1,15 +1,27 @@
 'use strict';
 
 addEventListener("message", function(e) {
-	let res = [],
-		d1 = new Date("January 30, 2019 13:00:00"),
-		d2 = new Date("July 30, 2018 13:00:00")
-	res.push(d1.getTimezoneOffset()+ ' | ' + d2.getTimezoneOffset())
-	res.push(Intl.DateTimeFormat().resolvedOptions().timeZone)
-	res.push(navigator.languages)
-	res.push(navigator.language)
-	res.push(navigator.languages[0])
-	res.push(new Intl.PluralRules().resolvedOptions().locale)
-	res.push(Intl.DateTimeFormat().resolvedOptions().locale)
+	let d1 = new Date("January 30, 2019 13:00:00"),
+		d2 = new Date("July 30, 2018 13:00:00"),
+		res = []
+	function get_item(item) {
+		try {
+			if (item == 0) {return navigator.languages
+			} else if (item == 1) {return navigator.language
+			} else if (item == 2) {return navigator.languages[0]
+			} else if (item == 3) {return new Intl.PluralRules().resolvedOptions().locale
+			} else if (item == 4) {return Intl.DateTimeFormat().resolvedOptions().locale
+			} else if (item == 5) {return d1.getTimezoneOffset()+ ' | ' + d2.getTimezoneOffset()
+			} else if (item == 6) {return Intl.DateTimeFormat().resolvedOptions().timeZone}
+		} catch(e) {
+			if (e.name = "ReferenceError") {return zB1} else {return zB2}
+		}
+	}
+	// build
+	for (let i=0; i < 7; i++) {
+		let result = get_item(i)
+		res.push(result)
+	}
+	// post
 	self.postMessage(res)
 }, false)
